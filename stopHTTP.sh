a01476738aea4b6df7ea813d954e53d586f8a450
denis lussier <denis@lussier.io>
2023-01-06 04:28:13 -0500

    fix bug where it was creating a file name ' '

--- stopHTTP.sh
+++ stopHTTP.sh
@@ -1,10 +1,9 @@
 pid=`ps aux | grep [h]ttp.server | awk '{print $2}'`
 
-if [ "$pid" > " " ]; then
+if [ ! "$pid" == "" ]; then
   echo "killing ($pid)"
   kill -9 $pid > /dev/null 2>&1
 else
   echo "http.server not running"
 fi
-
 
