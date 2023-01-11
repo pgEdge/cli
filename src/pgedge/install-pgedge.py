4724d46bba913e4c45cb3920e78120feabf20da0
denis lussier <denis@lussier.io>
2023-01-11 17:36:09 +0000

    more hardening when installing pgedge

--- src/pgedge/install-pgedge.py
+++ src/pgedge/install-pgedge.py
@@ -3,32 +3,59 @@ import util
 import os, sys
 
 thisDir = os.path.dirname(os.path.realpath(__file__))
-
 pgeV="pg15"
 
 withPOSTGREST = str(os.getenv("withPOSTGREST", "False"))
 withBACKREST  = str(os.getenv("withBACKREST", "False"))
 withBOUNCER   = str(os.getenv("withBOUNCER", "False"))
 
+
+def osSys(cmd):
+  print('#')
+  print('# ' + str(cmd))
+  os.system(cmd)
+
+
+## MAINLINE #####################################################3
+
+try:
+  import fire
+except ImportError as e:
+  osSys("pip3 install fire")
+
+try:
+  import psycopg2
+except ImportError as e:
+  osSys("pip3 install psycopg2")
+
 print(" ")
 print("## Install PgEdge for " + pgeV + " #######################################")
 
-os.system("./nc install " + pgeV + " --start ")
-os.system("./nc install spock")
+if os.path.isdir(pgeV):
+  print(" ")
+  print("# " + pgeV + " installation found.")
+else:
+  osSys("./nc install " + pgeV)
 
-print(" ")
-os.system("./nc tune " + pgeV)
+osSys("./nc start " + pgeV)
+osSys("./nc tune " + pgeV)
+
+db1 = os.getenv('pgName', '')
+if db1 > '':
+  db1 = " -d " + db1
+
+osSys("./nc install spock" + db1)
 
 if withPOSTGREST == "True":
   print(" ")
-  os.system("./nc install postgrest")
+  osSys("./nc install postgrest" + db1)
 
 if withBACKREST == "True":
   print(" ")
-  os.system("./nc install backrest")
+  osSys("./nc install backrest" + db1)
 
 if withBOUNCER == "True":
   print(" ")
-  os.system("./nc install bouncer")
+  os.system("./nc install bouncer" + db1)
 
 
