73e0351ffcce2b5c44179f964dc6bbec51ee9a90
denis lussier <denis@lussier.io>
2023-02-03 05:41:22 +0000

    more wip backrest

--- src/backrest/install-backrest.py
+++ src/backrest/install-backrest.py
@@ -3,51 +3,59 @@ import util
 import os, sys, random, subprocess
 
 thisDir = os.path.dirname(os.path.realpath(__file__))
-homeDir = os.getcwd()
 
-print(f"DEBUG:  cwd={os.getcwd()}")
-print(f"DEBUG: thisDir={thisDir}")
+def osSys(p_input, p_display=True):
+  if p_display:
+    util.message("# " + p_input)
+  rc = os.system(p_input)
+  return(rc)
+
 
 pgV="pg15"
 if not os.path.isdir(pgV):
-  print("installing " + pgV + " as a pre-req")
-  os.system("./nc install " + pgV + " --start")
+  util.message("/n## Installing " + pgV + " as a pre-req")
+  osSys("./nc install " + pgV + " --start")
 
 os.chdir(thisDir)
+osUsr = util.get_user()
+usrUsr = osUsr + ":" + osUsr
 
-os.system("rm -r lib")
-os.system("rm -r share")
+osSys("rm -r lib", False)
+osSys("rm -r share", False)
 
-os.system("sudo cp bin/pgbackrest  /usr/bin/.")
-os.system("sudo chmod 755 /usr/bin/pgbackrest")
-os.system("sudo mkdir -p -m 770 /var/log/pgbackrest")
+osSys("sudo cp bin/pgbackrest  /usr/bin/.")
+osSys("sudo chmod 755 /usr/bin/pgbackrest")
+osSys("sudo mkdir -p -m 770 /var/log/pgbackrest")
 
-usrUsr = util.get_user() + ":" + util.get_user()
+osSys("sudo chown " + usrUsr + " /var/log/pgbackrest")
+osSys("sudo mkdir -p /etc/pgbackrest")
+osSys("sudo mkdir -p /etc/pgbackrest/conf.d")
+osSys("sudo touch /etc/pgbackrest/pgbackrest.conf")
+osSys("sudo chmod 640 /etc/pgbackrest/pgbackrest.conf")
+osSys("sudo chown " + usrUsr + " /etc/pgbackrest/pgbackrest.conf")
 
-os.system("sudo chown " + usrUsr + " /var/log/pgbackrest")
-os.system("sudo mkdir -p /etc/pgbackrest")
-os.system("sudo mkdir -p /etc/pgbackrest/conf.d")
-os.system("sudo touch /etc/pgbackrest/pgbackrest.conf")
-os.system("sudo chmod 640 /etc/pgbackrest/pgbackrest.conf")
-os.system("sudo chown " + usrUsr + " /etc/pgbackrest/pgbackrest.conf")
-
-os.system("sudo mkdir -p /var/lib/pgbackrest")
-os.system("sudo chmod 750 /var/lib/pgbackrest")
-os.system("sudo chown " + usrUsr + " /var/lib/pgbackrest")
+osSys("sudo mkdir -p /var/lib/pgbackrest")
+osSys("sudo chmod 750 /var/lib/pgbackrest")
+osSys("sudo chown " + usrUsr + " /var/lib/pgbackrest")
 
 dataDir= os.getenv("MY_HOME") + "/data/pg15"
 
+util.message("\n## creating '/etc/pgbackrest/pgbackrest.conf' ########")
 conf_file=thisDir + "/pgbackrest.conf"
 util.replace("pgXX", pgV, conf_file, True)
 util.replace("pg1-path=xx", "pg1-path=" + dataDir, conf_file, True)
+util.replace("pg1-user=xx", "pg1-user=" + osUsr, conf_file, True)
+util.replace("pg1-database=xx", "pg1-database=" + "postgres", conf_file, True)
 
-bCipher = subprocess.check_output("dd if=/dev/urandom bs=256 count=1 2> /dev/null | LC_ALL=C tr -dc 'A-Za-z0-9' | head -c32", shell=True)
+cmd="dd if=/dev/urandom bs=256 count=1 2> /dev/null | LC_ALL=C tr -dc 'A-Za-z0-9' | head -c32"
+bCipher = subprocess.check_output(cmd, shell=True)
 sCipher = bCipher.decode('ascii')
 util.replace("repo1-cipher-pass=xx", "repo1-cipher-pass=" + sCipher, conf_file, True)
+osSys("cp " + conf_file + "  /etc/pgbackrest/.")
 
+util.message("\n## Modify 'postgresql.conf' #########################")
 aCmd = "pgbackrest --stanza=" + pgV + " archive-push %p"
-util.change_pgconf_keyval(pgV, "archive_command", aCmd)
+util.change_pgconf_keyval(pgV, "archive_command", aCmd, p_replace=True)
 util.change_pgconf_keyval(pgV, "archive_mode", "on", p_replace=True)
 
-os.system("cp " + conf_file + "  /etc/pgbackrest/.")
 
