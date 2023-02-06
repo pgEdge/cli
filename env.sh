cc72d076fced14182acd041aac150025441af9e2
denis lussier <denis@lussier.io>
2023-02-06 17:48:13 +0000

    introducing bumpHub.sh...

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 
 bundle=pgedge
 api=nc
-hubV=2.18
+hubV=2.19
 
 spockV=3.0.16-1
 #spockV=3.0da-0106-1
--- src/conf/versions.sql
+++ src/conf/versions.sql
@@ -125,10 +125,7 @@ INSERT INTO categories VALUES (9,  87, 'Management & Monitoring', 'Manage/Monito
 -- ## HUB ################################
 INSERT INTO projects VALUES ('hub', 'app', 0, 0, 'hub', 0, 'https://github.com/pgedge/nodectl','',0,'','','');
 INSERT INTO releases VALUES ('hub', 1, 'hub', '', '', 'hidden', '', 1, '', '', '');
-INSERT INTO versions VALUES ('hub', '2.18', '',  1, '20230205', '', '', '');
-INSERT INTO versions VALUES ('hub', '2.17', '',  0, '20230203', '', '', '');
-INSERT INTO versions VALUES ('hub', '2.16', '',  0, '20230201', '', '', '');
-INSERT INTO versions VALUES ('hub', '2.15', '',  0, '20230131', '', '', '');
+INSERT INTO versions VALUES ('hub', '2.19', '',  1, '20230307', '', '', '');
 
 -- ##
 INSERT INTO projects VALUES ('pg', 'pge', 1, 5432, 'hub', 1, 'https://github.com/postgres/postgres/tags',
