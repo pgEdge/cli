40d36c667b509b38ee61a74213a307f20f85e83e
denis lussier <denis@lussier.io>
2023-02-13 10:03:54 -0500

    improve install-spock to better match hari's spec

--- src/spock-pgXX/install-spock-pgXX.py
+++ src/spock-pgXX/install-spock-pgXX.py
@@ -5,10 +5,14 @@
 
 import util, datetime, os
 
+max_worker_procs = int(util.get_cpu_cores() * .75) + 6
+if max_worker_procs < 10:
+  max_worker_procs = 10
+
 util.change_pgconf_keyval("pgXX", "wal_level", "logical", True)
-util.change_pgconf_keyval("pgXX", "max_worker_processes", "10", True)
-util.change_pgconf_keyval("pgXX", "max_replication_slots", "10", True)
-util.change_pgconf_keyval("pgXX", "max_wal_senders", "10", True)
+util.change_pgconf_keyval("pgXX", "max_worker_processes", str(max_worker_procs), True)
+util.change_pgconf_keyval("pgXX", "max_replication_slots", "16", True)
+util.change_pgconf_keyval("pgXX", "max_wal_senders", "16", True)
 
 util.change_pgconf_keyval("pgXX", "track_commit_timestamp", "on", True)
 #util.change_pgconf_keyval("pgXX", "log_min_messages", "debug3", True)
