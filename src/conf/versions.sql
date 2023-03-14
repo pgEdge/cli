17db6953dedb3d3477241258426cb7c9cb37a902
denis lussier <denis@lussier.io>
2023-03-14 11:40:45 -0400

    spock 3.0.22-2 & 1st pass at --location

--- src/conf/versions.sql
+++ src/conf/versions.sql
@@ -338,7 +338,7 @@ INSERT INTO versions VALUES ('timescaledb-pg14', '2.8.0-1',  'el8, arm', 0, '202
 INSERT INTO projects VALUES ('spock', 'pge', 4, 0, 'hub', 1, 'https://github.com/pgedge/spock/tags',
   'spock', 1, 'spock.png', 'Logical & Multi-Active Replication', 'https://github.com/pgedge/spock/#spock');
 INSERT INTO releases VALUES ('spock-pg15', 4, 'spock', 'Spock', '', 'prod', '', 1, 'pgEdge Community', '', '');
-INSERT INTO versions VALUES ('spock-pg15', '3.0.22-1', 'el8, arm, osx', 1, '20230314', 'pg15', '', '');
+INSERT INTO versions VALUES ('spock-pg15', '3.0.22-2', 'el8, arm, osx', 1, '20230314', 'pg15', '', '');
 INSERT INTO versions VALUES ('spock-pg15', '3.0.21-1', 'el8, arm, osx', 0, '20230310', 'pg15', '', '');
 INSERT INTO versions VALUES ('spock-pg15', '3.0.19-1', 'el8, arm, osx', 0, '20230301', 'pg15', '', '');
 
