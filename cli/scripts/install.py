95d21fa1c6b571c9d3b8fe281c2ff7a3416de8ff
denis lussier <denis@lussier.io>
2023-03-24 16:50:35 -0400

    bump to 23.107

--- cli/scripts/install.py
+++ cli/scripts/install.py
@@ -4,7 +4,7 @@
 
 import sys, os
 
-VER="23.106"
+VER="23.107"
 REPO=os.getenv("REPO", "https://pgedge-upstream.s3.amazonaws.com/REPO")
   
 if sys.version_info < (3, 6):
--- src/conf/versions.sql
+++ src/conf/versions.sql
@@ -126,10 +126,10 @@ INSERT INTO categories VALUES (9,  87, 'Management & Monitoring', 'Manage/Monito
 -- ## HUB ################################
 INSERT INTO projects VALUES ('hub', 'app', 0, 0, 'hub', 0, 'https://github.com/pgedge/nodectl','',0,'','','');
 INSERT INTO releases VALUES ('hub', 1, 'hub', '', '', 'hidden', '', 1, '', '', '');
-INSERT INTO versions VALUES ('hub', '23.106', '',  1, '20230324', '', '', '');
+INSERT INTO versions VALUES ('hub', '23.107', '',  1, '20230327', '', '', '');
+INSERT INTO versions VALUES ('hub', '23.106', '',  0, '20230324', '', '', '');
 INSERT INTO versions VALUES ('hub', '23.105', '',  0, '20230324', '', '', '');
 INSERT INTO versions VALUES ('hub', '23.103', '',  0, '20230310', '', '', '');
-INSERT INTO versions VALUES ('hub', '23.102', '',  0, '20230304', '', '', '');
 
 -- ##
 INSERT INTO projects VALUES ('pg', 'pge', 1, 5432, 'hub', 1, 'https://github.com/postgres/postgres/tags',
