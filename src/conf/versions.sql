db3d7d13c7e00884e2b7b596bf196f84fec19134
denis Lussier <denis@lussier.io>
2023-04-24 23:24:20 +0000

    bump hub to 23.112

--- src/conf/versions.sql
+++ src/conf/versions.sql
@@ -126,12 +126,10 @@ INSERT INTO categories VALUES (9,  87, 'Management & Monitoring', 'Manage/Monito
 -- ## HUB ################################
 INSERT INTO projects VALUES ('hub', 'app', 0, 0, 'hub', 0, 'https://github.com/pgedge/nodectl','',0,'','','');
 INSERT INTO releases VALUES ('hub', 1, 'hub', '', '', 'hidden', '', 1, '', '', '');
-INSERT INTO versions VALUES ('hub', '23.111', '',  1, '20230425', '', '', '');
+INSERT INTO versions VALUES ('hub', '23.112', '',  1, '20230427', '', '', '');
+INSERT INTO versions VALUES ('hub', '23.111', '',  0, '20230424', '', '', '');
 INSERT INTO versions VALUES ('hub', '23.110', '',  0, '20230418', '', '', '');
 INSERT INTO versions VALUES ('hub', '23.109', '',  0, '20230404', '', '', '');
-INSERT INTO versions VALUES ('hub', '23.108', '',  0, '20230328', '', '', '');
-INSERT INTO versions VALUES ('hub', '23.107', '',  0, '20230328', '', '', '');
-INSERT INTO versions VALUES ('hub', '23.106', '',  0, '20230324', '', '', '');
 
 -- ##
 INSERT INTO projects VALUES ('pg', 'pge', 1, 5432, 'hub', 1, 'https://github.com/postgres/postgres/tags',
