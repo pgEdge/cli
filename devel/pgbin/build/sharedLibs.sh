3fe685a261c756fe1669a033f895e82ba4e4f93b
denis Lussier <denis@lussier.io>
2023-04-25 16:27:19 +0000

    add libeconf to shared libs

--- devel/pgbin/build/sharedLibs.sh
+++ devel/pgbin/build/sharedLibs.sh
@@ -41,6 +41,7 @@ function linuxCopySharedLibs {
   cp -Pv $lib/libcap*            $shared_lib/.
   cp -Pv $lib/libaudit*          $shared_lib/.
   cp -Pv $lib/libresolv*         $shared_lib/.
+  cp -Pv $lib/libeconf*          $shared_lib/.
   cp -Pv $lib/liblzma.so.*       $shared_lib/.
   cp -Pv $lib/libcom_err.so.*    $shared_lib/.
   cp -Pv $lib/libkeyutils.so.*   $shared_lib/.
