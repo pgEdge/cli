33fdc5d9d910a1db0210810c9a1da0041399219e
Cady Motyka <cadymo@Cadys-MacBook-Pro.local>
2023-05-16 16:39:16 -0400

    First pass at ACE diff-spock

--- cli/scripts/ace.py
+++ cli/scripts/ace.py
@@ -5,11 +5,84 @@
 """ACE is the place of the Anti Chaos Engine"""
 
 import os, sys, random, time, json, socket, subprocess
-import psycopg
 import util, fire, meta, pgbench, cluster
 
 l_dir = "/tmp"
 
+try:
+  import psycopg
+except ImportError as e:
+  util.exit_message("Missing 'psycopg' module from pip", 1)
+
+
+def get_pg_v(pg):
+  pg_v = str(pg)
+
+  if pg_v.isdigit():
+    pg_v = "pg" + str(pg_v)
+
+  if pg_v == "None":
+    k = 0
+    pg_s = meta.get_installed_pg()
+
+    for p in pg_s:
+      k = k + 1
+
+    if k == 1:
+      pg_v = str(p[0])
+    else:
+      util.exit_message("must be one PG installed", 1)
+
+  if not os.path.isdir(pg_v):
+    util.exit_message(str(pg_v) + " not installed", 1)
+
+  return(pg_v)
+
+
+def get_pg_connection(pg_v, db, ip, usr):
+  dbp = util.get_column("port", pg_v)
+
+  if util.debug_lvl() > 0:
+    util.message(f"get_pg_connection(): dbname={db}, user={usr}, port={dbp}", "debug")
+
+  try:
+    con = psycopg.connect(dbname=db, user=usr, host=ip, port=dbp, autocommit=False)
+  except Exception as e:
+    util.exit_exception(e)
+
+  return(con)
+
+def run_psyco_sql(pg_v, db, cmd, ip, usr=None):
+  if usr == None:
+    usr = util.get_user()
+
+  if util.is_verbose():
+    util.message(cmd, "info")
+
+  if util.debug_lvl() > 0:
+    util.message(cmd, "debug")
+
+  con = get_pg_connection(pg_v, db, ip, usr)
+  if util.debug_lvl() > 0:
+    util.message("run_psyco_sql(): " + str(con), "debug")
+
+  try:
+    cur = con.cursor(row_factory=psycopg.rows.dict_row)
+    cur.execute(cmd)
+    con.commit()
+
+    return cur.fetchall()
+    try:
+      cur.close()
+      con.close()
+    except Exception as e:
+      pass
+
+  except Exception as e:
+      util.exit_exception(e)
+
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
 
 def get_csv_file_name(p_prfx, p_schm, p_tbl, p_base_dir="/tmp"):
   return(p_base_dir + os.sep + p_prfx + "-" + p_schm + "-" + p_tbl + ".csv")
@@ -103,9 +176,88 @@ def diff_schemas():
   pass
 
 
-def diff_spock():
+def diff_spock(cluster_name, node1, node2, pg=None):
   """Compare spock setup on different cluster nodes"""
-  pass
+  util.check_cluster_exists(cluster_name)
+
+  if node1 == node2:
+    util.exit_message("node1 must be different than node2")
+
+  db, pg, count, usr, cert, cluster_nodes = cluster.load_json(cluster_name)
+  compare_spock=[]
+  pg_v = get_pg_v(pg)
+  print("\n")
+
+  for cluster_node in cluster_nodes:
+    if cluster_node["nodename"] not in [node1, node2]:
+      continue
+    diff_spock={}
+    diff_sub={}
+    hints=[]
+    print(" Spock - Config " + cluster_node["nodename"])
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~")
+    prCyan("Node:")
+    sql = """
+    SELECT n.node_id, n.node_name, n.location, n.country,
+           s.sub_id, s.sub_name, s.sub_enabled, s.sub_replication_sets
+           FROM spock.node n LEFT OUTER JOIN spock.subscription s
+           ON s.sub_target=n.node_id WHERE s.sub_name IS NOT NULL;
+    """
+    node_info = run_psyco_sql(pg_v, db, sql, cluster_node["ip"])
+    print("  " + node_info[0]["node_name"])
+    diff_spock["node"]=node_info[0]["node_name"]
+
+    prCyan("  Subscriptions:")
+
+    for node in node_info:
+      if node["sub_name"] == None:
+        hints.append("Hint: No subscriptions have been created on this node")
+      else:
+        print("    " + node["sub_name"])
+        diff_sub["sub_name"]=node["sub_name"]
+        diff_sub["sub_enabled"]=node["sub_enabled"]
+        prCyan("    RepSets:")
+        diff_sub["replication_sets"]=node["sub_replication_sets"]
+        print("      " + json.dumps(node["sub_replication_sets"]))
+        if node["sub_replication_sets"] == []:
+          hints.append("Hint: No replication sets added to subscription")
+        elif node["sub_replication_sets"] == ["default", "default_insert_only", "ddl_sql"]:
+          hints.append("Hint: Only default replication sets added to subscription: " + node["sub_name"])
+        diff_spock["subscriptions"]= diff_sub
+
+    sql = """
+    SELECT set_name, string_agg(relname,'   ') as relname FROM spock.tables GROUP BY set_name ORDER BY set_name;
+    """
+    table_info = run_psyco_sql(pg_v, db, sql, cluster_node["ip"])
+    diff_spock["rep_set_info"]=[]
+    prCyan("Tables in RepSets:")
+    if table_info==[]:
+        hints.append("Hint: No tables in database")
+    for table in table_info:
+      if table["set_name"] == None:
+        print(" - Not in a replication set")
+        hints.append("Hint: Tables not in replication set might not have primary keys, or you need to run repset-add-table")
+      else:
+        print(" - " + table["set_name"])
+      
+      diff_spock["rep_set_info"].append({table["set_name"]:table["relname"]})
+      print("   - " + table["relname"])
+        
+    compare_spock.append(diff_spock)
+    for hint in hints:
+      prRed(hint)
+    print("\n")
+
+  if len(compare_spock) == 2: 
+    print(" Spock - Diff") 
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~")
+    if compare_spock[0]["rep_set_info"] == compare_spock[1]["rep_set_info"]:
+      prCyan("   Replication Rules are the same!!")
+    else:
+      prRed("    Difference in Replication Rules")
+  
+  ##print(json.dumps(compare_spock,indent=2))
+  return(compare_spock)
 
 
 def diff_tables(cluster_name, node1, node2, table_name):
@@ -121,9 +273,9 @@ def diff_tables(cluster_name, node1, node2, table_name):
   if node1 == node2:
     util.exit_message("node1 must be different than node2")
 
-  util.message(f"## Validating nodes {node1} & {node2} exist\n")
-  util.check_node_exists(cluster_name, node1)
-  util.check_node_exists(cluster_name, node2)
+  ##util.message(f"## Validating nodes {node1} & {node2} exist\n")
+  ##util.check_node_exists(cluster_name, node1)
+  ##util.check_node_exists(cluster_name, node2)
 
   nm_lst = table_name.split(".")
   if len(nm_lst) != 2:
@@ -183,7 +335,6 @@ def diff_tables(cluster_name, node1, node2, table_name):
     rc = 0
 
   return(rc)
-
 
 
 if __name__ == '__main__':
