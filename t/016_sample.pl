c7b1e09e076f60b9f1b67dd298e33f43f8c1f867
Susan Douglas <susan@pgedge.com>
2023-06-13 09:40:15 -0400

    Updated test sample

--- t/016_sample.pl
+++ t/016_sample.pl
@@ -15,13 +15,16 @@ use Test::More tests => 1;
 use Try::Tiny;
 
 my $nc = NodeCtl::get_new_nc("");
-my $cmd = qq(psql -h 127.0.0.1 -d postgres -c "CREATE ROLE moo LOGIN PASSWORD 'password'");
+my $cmd = qq(psql -h 127.0.0.1 -d postgres -c "CREATE ROLE hoo LOGIN PASSWORD 'password'");
 my ($success, $error_message, $full_buf, $stdout_buf, $stderr_buf)= $nc->exec_command($cmd);
-diag ("$success");
-diag ("$error_message");
-diag ("full_buf contains @$full_buf");
-diag ("stdout contains @$stdout_buf");
-diag ("stderr contains @$stderr_buf");
+ 
+if ("$full_buf" == 'CREATE ROLE')
+{
+    ok(1);
+}
+else
+{
+    ok(0);
+}
 
-ok(1);
 done_testing();
