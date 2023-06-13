2c021b513289275a557f7006dc4f2d9dfbad666e
denis lussier <denis@lussier.io>
2023-06-13 00:14:07 +0000

    add paho-mqtt to seperated out requirements.txt

--- devel/setup/1b-requirements.sh
+++ devel/setup/1b-requirements.sh
@@ -1,9 +1,7 @@
 
+set -x
+
 sudo dnf install -y python3-psutil
 
-pip3i="pip3 install --user"
+pip3 install --user -r $NC/requirements.txt
 
-$pip3i patroni[etcd3]
-$pip3i paramiko
-$pip3i psycopg
-$pip3i psycopg-binary
