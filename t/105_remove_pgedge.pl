4d9ba9f42061e2f9a5eb8d05254f660d00840fbe
Susan Douglas <susan@pgedge.com>
2023-06-22 11:45:19 -0400

    Added test 105_remove_pgedge.pl; this test removes the artifacts installed during test 100_install_pgedge.pl

---
+++ t/105_remove_pgedge.pl
@@ -0,0 +1,61 @@
+# This test case cleans up after the test: 100_install_pgedge.pl.  
+# We remove the PG installation, the pgedge directory, and the  ~/.pgpass file.
+#
+
+use strict;
+use warnings;
+
+use File::Which;
+use PostgreSQL::Test::Cluster;
+use PostgreSQL::Test::Utils;
+use Test::More tests => 1;
+use IPC::Cmd qw(run);
+use Try::Tiny;
+use JSON;
+use NodeCtl;
+
+#
+# Get the location of the data directory and home directory before removing pgEdge; store them in $datadir and $home.
+#
+
+my $nc = NodeCtl::get_new_nc("/tmp/nodectl_dir");
+my $datadir = $nc->get_info_item_pg16("datadir");
+my $home = $nc->get_home_dir();
+
+#diag("datadir = $datadir\n");
+#diag("home = $home\n");
+
+#
+# Then, use nodectl to remove the Postgres installation.
+#
+
+my $cmd = qq(./nodectl remove pgedge);
+#diag("cmd = $cmd\n");
+my ($success, $error_message, $full_buf, $stdout_buf, $stderr_buf)= IPC::Cmd::run(command => $cmd, verbose => 0);
+
+#
+# Then, remove the data directory and the contents of the pgedge directory; then the pgedge directory is deleted.
+#
+
+File::Path::remove_tree($datadir);
+
+my $result = system("rm -rf $home");
+
+#
+# Then, we remove the ~/.pgpass file.
+#
+
+my $cmd1 = qq(sudo rm ~/.pgpass);
+#diag("cmd1 = $cmd1");
+my ($success1, $error_message1, $full_buf1, $stdout_buf1, $stderr_buf1)= IPC::Cmd::run(command => $cmd1, verbose => 0);
+
+if (defined($success))
+{
+    ok(1);
+}
+else
+{
+    ok(0);
+}
+
+done_testing();
