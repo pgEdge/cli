ecbf22625b381cc7cf115db4000d8d6150c09a3c
denis lussier <denis@lussier.io>
2023-07-02 10:27:59 -0400

    wip pgbench for remote & localhost

--- cli/scripts/pgbench.py
+++ cli/scripts/pgbench.py
@@ -4,37 +4,45 @@
 
 import util, cluster
 
-def setup_node(node_nm, port, nc, num_nodes, db, pg, host, factor):
+def setup_node(node_nm, port, nc, num_nodes, db, pg, host, factor, os_user, ssh_key):
   pgb = nc + " pgbin "
   spk = nc + " spock "
   scale = int(num_nodes) * int(factor)
   pgbench_cmd = '"pgbench --initialize --scale=' + str(scale) + ' ' + str(db) + '"'
-  util.echo_cmd(pgb + str(pg) +  " " + pgbench_cmd, host=host)
+  util.echo_cmd(pgb + str(pg) +  " " + pgbench_cmd, host=host, usr=os_user, key=ssh_key)
 
   dsn = "'host=127.0.0.1 port=" + str(port) + " dbname=" + db + "'"
-  util.echo_cmd(spk + "node-create " + node_nm + " --dsn " + dsn + " --db " + db, host=host)
+  util.echo_cmd(spk + "node-create " + node_nm + " --dsn " + dsn + " --db " + db,
+                 host=host, usr=os_user, key=ssh_key)
 
   rep_set = 'pgbench-repset'
-  util.echo_cmd(spk + "repset-create " + rep_set + " --db " + db, host=host)
-  util.echo_cmd(spk + "repset-add-table " + rep_set + " public.pgbench* --db " + db, host=host)
+  util.echo_cmd(spk + "repset-create " + rep_set + " --db " + db,
+                  host=host, usr=os_user, key=ssh_key)
+  util.echo_cmd(spk + "repset-add-table " + rep_set + " public.pgbench* --db " + db,
+                  host=host, usr=os_user, key=ssh_key)
 
-  log_old_val("pgbench_accounts", "abalance", "true", nc, db, pg)
-  log_old_val("pgbench_branches", "bbalance", "true", nc, db, pg)
-  log_old_val("pgbench_tellers",  "tbalance", "true", nc, db, pg)
+  log_old_val("pgbench_accounts", "abalance", "true", nc, db, pg,
+                host=host, usr=os_user, key=ssh_key)
+  log_old_val("pgbench_branches", "bbalance", "true", nc, db, pg,
+                host=host, usr=os_user, key=ssh_key)
+  log_old_val("pgbench_tellers",  "tbalance", "true", nc, db, pg,
+                host=host, usr=os_user, key=ssh_key)
 
 
-def psql_cmd(cmd, nc, db, pg):
-    util.echo_cmd(nc + "psql " + str(pg) + " \"" + cmd + "\" " + db)
+def psql_cmd(cmd, nc, db, pg, host, usr, key):
+  util.echo_cmd(nc + "psql " + str(pg) + " \"" + cmd + "\" " + db,
+                  host, usr, key)
 
 
-def log_old_val(tbl, col, val, nc, db, pg):
-    cmd = "ALTER TABLE " + tbl + " ALTER COLUMN " + col + " SET (LOG_OLD_VALUE=" + val + ")"
-    psql_cmd(cmd, nc, db, pg)
+def log_old_val(tbl, col, val, nc, db, pg, host, usr, key):
+  cmd = "ALTER TABLE " + tbl + " ALTER COLUMN " + col + \
+    " SET (LOG_OLD_VALUE=" + val + ")"
+  psql_cmd(cmd, nc, db, pg, host, usr, key)
 
 
 def install(cluster_name, factor=1):
   util.message("\n# loading cluster definition ######")
-  db, pg, count, db_user, db_passwd, os_user, cert, nodes = cluster.load_json(cluster_name)
+  db, pg, count, db_user, db_passwd, os_user, ssh_key, nodes = cluster.load_json(cluster_name)
   db_pg = " " + str(db) + " --pg=" + str(pg)
 
   util.message("\n# setup individual nodes ##########")
@@ -43,7 +51,8 @@ def install(cluster_name, factor=1):
     port = nd["port"]
     host = nd["ip"]
     nc = nd["path"] + "/nodectl "
-    setup_node(nodename, port, nc, count, db, str(pg), host, factor)
+    setup_node(nodename, port, nc, count, db, str(pg), host, factor,
+                 os_user=os_user, ssh_key=ssh_key)
 
   util.message("\n# wire nodes together #############")
   for pub in nodes:
@@ -58,12 +67,14 @@ def install(cluster_name, factor=1):
         sub_name = "sub_" + pub["nodename"] + sub["nodename"] + " "
         provider_dsn = "'" + sub_ip_port + " user=" + db_user + " dbname=" + db + "' "
 
-        util.echo_cmd(spk + "sub-create " + sub_name + provider_dsn + db_pg, host=host)
-        util.echo_cmd(spk + "sub-add-repset " + sub_name + " pgbench-repset " + db_pg, host=host)
+        util.echo_cmd(spk + "sub-create " + sub_name + provider_dsn + db_pg, 
+                        host=host, usr=os_user, key=ssh_key)
+        util.echo_cmd(spk + "sub-add-repset " + sub_name + " pgbench-repset " + db_pg, 
+                        host=host, usr=os_user, key=ssh_key)
 
 
 def remove(cluster_name):
-  db, pg, count, usr, cert, nodes = cluster.load_json(cluster_name)
+  db, pg, count, db_usr, db_passwd, os_user, ssh_key, nodes = cluster.load_json(cluster_name)
   db_pg = " " + str(db) + " --pg=" + str(pg)
 
   for pub in nodes:
@@ -76,16 +87,23 @@ def remove(cluster_name):
 
       if pub_ip_port != sub_ip_port:
         sub_name = "sub_" + pub["nodename"] + sub["nodename"] + " "
-        util.echo_cmd(spk + "sub-drop " + sub_name + db_pg, host=host)
+        util.echo_cmd(spk + "sub-drop " + sub_name + db_pg, 
+                        host=host, usr=os_user, key=ssh_key)
 
   for nd in nodes:
     host = nd["ip"]
     nc =nd["path"] + "/nodectl "
     spk = nc + "spock "
-    util.echo_cmd(spk + "repset-drop pgbench-repset" + db_pg, host=host)
-    util.echo_cmd(spk + "node-drop " + nd["nodename"] + db_pg, host=host)
-    psql_cmd("DROP TABLE pgbench_history",  nc, db, pg)
-    psql_cmd("DROP TABLE pgbench_accounts", nc, db, pg)
-    psql_cmd("DROP TABLE pgbench_tellers",  nc, db, pg)
-    psql_cmd("DROP TABLE pgbench_branches", nc, db, pg)
+    util.echo_cmd(spk + "repset-drop pgbench-repset" + db_pg, 
+                    host=host, usr=os_user, key=ssh_key)
+    util.echo_cmd(spk + "node-drop " + nd["nodename"] + db_pg, 
+                    host=host, usr=os_user, key=ssh_key)
+    psql_cmd("DROP TABLE pgbench_history",  nc, db, pg,
+                host=host, usr=os_user, key=ssh_key)
+    psql_cmd("DROP TABLE pgbench_accounts", nc, db, pg,
+                host=host, usr=os_user, key=ssh_key)
+    psql_cmd("DROP TABLE pgbench_tellers",  nc, db, pg,
+                host=host, usr=os_user, key=ssh_key)
+    psql_cmd("DROP TABLE pgbench_branches", nc, db, pg,
+                host=host, usr=os_user, key=ssh_key)
  
