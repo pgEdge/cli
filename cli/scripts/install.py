b4a696900a19ef3061b9f4d2b83c1693254b6eb0
denis lussier <denis@lussier.io>
2023-07-07 18:32:24 -0400

    1st pass at refactoring of install.py tobe 3x faster AND run on both AMD&ARM OSX clients

--- cli/scripts/install.py
+++ cli/scripts/install.py
@@ -16,7 +16,39 @@ if sys.version_info < (3, 9):
 
 from urllib import request as urllib2
 
-import tarfile
+import tarfile, platform
+
+
+def download_n_unpack(p_file, p_url, p_download_msg, p_del_download):
+  if os.path.exists(p_file):
+    os.system("rm -f " + p_file)
+
+  print("Downloading " + str(p_download_msg))
+  f = str(p_url) + "/" + p_file
+  try:
+    fu = urllib2.urlopen(f)
+    local_file = open(p_file, "wb")
+    local_file.write(fu.read())
+    local_file.close()
+  except Exception as e:
+    print("ERROR: Unable to download " + f + "\n" + str(e))
+    sys.exit(1)
+
+  print("Unpacking ...")
+  try:
+    tar = tarfile.open(p_file)
+    tar.extractall(path=".")
+    tar.close()
+    if p_del_download == True:
+      os.remove(p_file)
+  except Exception as e:
+    print("ERROR: Unable to unpack \n" + str(e))
+    sys.exit(1)
+
+
+#######################
+# MAINLINE
+#######################
 
 IS_64BITS = sys.maxsize > 2**32
 if not IS_64BITS:
@@ -30,35 +62,39 @@ if os.path.exists("pgedge"):
     sys.exit(1)
 
 my_file="pgedge-nodectl-" + VER + ".tar.bz2"
-f = REPO + "/" + my_file
 
-if not os.path.exists(my_file):
-  print("Downloading CLI " + VER + " ...")
-  try:
-    fu = urllib2.urlopen(f)
-    local_file = open(my_file, "wb")
-    local_file.write(fu.read())
-    local_file.close()
-  except Exception as e:
-    print("ERROR: Unable to download " + f + "\n" + str(e))
-    sys.exit(1)
-
-print("Unpacking ...")
-try:
-  tar = tarfile.open(my_file)
-  tar.extractall(path=".")
-  tar.close()
-  os.remove(my_file)
-except Exception as e:
-  print("ERROR: Unable to unpack \n" + str(e))
-  sys.exit(1)
-
+download_n_unpack(my_file, REPO, "CLI " + VER + " ...", True)
 cmd = "pgedge" + os.sep + "nodectl "
 os.system(cmd + "set GLOBAL REPO " + REPO)
 os.system(cmd + "update --silent")
 os.system(cmd + "info")
 
-print("pgedge/nodectl installed.\n")
+## figure out what python-libs file we need ##############
+if platform.system() == "Linux":
+  if platform.machine() == "aarch64":
+    machine = "arm"
+  else:
+    machine = "amd"
+
+  if os.path.isfile("/etc/redhat-release"):
+    plat = "el9"
+  else:
+    plat = "ubu22"
+
+  lib_file = f"linux-{plat}-{machine}"
+
+elif platform.system() == "Darwin":
+  lib_file = "osx-amd"
+
+else:
+  print("ERROR: platform.system() {platform.system()} not supported.")
+  sys.exit(1)
+
+lib_download = f"lib-{lib_file}.tar.bz2"
+print(f"\nDownloading supporting libs '{lib_download}' ...")
+##download_n_unpack(my_file, REPO, "CLI " + VER + " ...", False)
+
+print("\npgedge/nodectl installed.\n")
 
 sys.exit(0)
 
