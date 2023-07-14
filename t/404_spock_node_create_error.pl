e661fb89d7ee08fe8259722913c4607df2f299cd
Susan Douglas <susan@pgedge.com>
2023-07-14 10:56:35 -0400

    Added tests 404/405/406/407 for spock_node_create errors and cleanup

---
+++ t/404_spock_node_create_error.pl
@@ -0,0 +1,72 @@
+# This test case walks through the steps in the Getting Started Guide to create a replicating two-node
+# cluster.  I'll use it as a starting point for tests that check spock error handling.
+#
+
+use strict;
+use warnings;
+
+use File::Which;
+use IPC::Cmd qw(run);
+use Try::Tiny;
+use JSON;
+
+#
+# Move into the pgedge directory.
+#
+ chdir("./pgedge");
+
+#
+# First, we use nodectl to create a two-node cluster named demo; the nodes are named n1/n2 (default names), 
+# the database is named lcdb (default), and it is owned by lcdb (default).
+# 
+
+my $cmd = qq(./nodectl cluster create-local demo 2 --pg 16);
+print("cmd = $cmd\n");
+my ($success, $error_message, $full_buf, $stdout_buf, $stderr_buf)= IPC::Cmd::run(command => $cmd, verbose => 0);
+
+print("full_buf = @$full_buf\n");
+print("stderr_buf = @$stderr_buf\n");
+print("This s/b a 2 node cluster named demo, owned by lcdb, with a db named lcdb.  The nodes are named n1/n2.\n");
+print("This test assumes they're running on 6432 and 6433\n");
+
+#
+# Register node 1 and the repset entry on n1: 
+#
+
+my $cmd2 = qq(./nodectl spock node-create n1 'the dsn is missing' lcdb);
+print("cmd2 = $cmd2\n");
+my ($success2, $error_message2, $full_buf2, $stdout_buf2, $stderr_buf2)= IPC::Cmd::run(command => $cmd2, verbose => 0);
+
+print("We just invoked the ./nc spock node-create n1 command but without a dsn.\n");
+
+#
+# Print statements
+#
+
+print("error_message2 = $error_message2\n");
+print("full_buf2 = @$full_buf2\n");
+print("stdout_buf2 = @$stdout_buf2\n");
+print("stderr_buf2 = @$stderr_buf2\n");
+
+#
+print("If the server reports an ERROR for the missing dsn, the test succeeded\n");
+#
+
+my $substring = "ERROR";
+if (index($stdout_buf2, $substring) == -1)
+
+{
+  exit(0);
+}
+else
+{
+ exit(1);
+}
+
+
+
+
+
+
+
+
---
+++ t/407_spock_error_breakdown_script.pl
@@ -0,0 +1,68 @@
+# This test case cleans up after the test: 405_spock_node_create_error.pl  
+# The test exercises: ./nodectl remove pgedge
+# We remove the PG installation, the pgedge directory, and the  ~/.pgpass file.
+#
+
+use strict;
+use warnings;
+
+use File::Which;
+use File::Path;
+use IPC::Cmd qw(run);
+use Try::Tiny;
+use JSON;
+
+#
+# Move into the pgedge directory
+#
+chdir ("./pgedge");
+
+#
+# Get the location of the data directory and home directory before removing pgEdge; store them in $datadir and $home.
+#
+
+my $out = decode_json(`./nc --json info`);
+my $home = $out->[0]->{"home"};
+print("the home directory is = {$home}\n");
+
+my $out1 = decode_json(`./nc --json info pg16`);
+my $datadir = $out1->[0]->{"datadir"};
+print("the data directory is = {$datadir}\n");
+
+print("datadir = $datadir\n");
+print("home = $home\n");
+
+#
+# Then, use nodectl to remove the Postgres installation.
+#
+
+my $cmd = qq(./nodectl remove pgedge);
+print("cmd = $cmd\n");
+my ($success, $error_message, $full_buf, $stdout_buf, $stderr_buf)= IPC::Cmd::run(command => $cmd, verbose => 0);
+
+#
+# Diagnostic print statements
+#
+
+print("success = $success\n");
+# print("error_message = $error_message\n");
+print("full_buf = @$full_buf\n");
+print("stdout_buf = @$stdout_buf\n");
+print("stderr_buf = @$stderr_buf\n");
+
+#
+# Then, remove the data directory and the contents of the pgedge directory; then the pgedge directory is deleted.
+#
+
+File::Path::remove_tree($datadir);
+
+my $result = system("rm -rf $home");
+
+if (defined($success))
+{
+    exit(0);
+}
+else
+{
+    exit(1);
+}
