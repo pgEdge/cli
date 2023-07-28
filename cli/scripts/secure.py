7084b9a59d3f6f4a38234fa74990d204f8ceb9a9
Cady Motyka <cadymo@Cadys-MacBook-Pro.local>
2023-07-28 14:12:32 -0400

    destroy cluster and wip import-cluster

--- cli/scripts/secure.py
+++ cli/scripts/secure.py
@@ -3,8 +3,8 @@
 #####################################################
 
 import os, sys, random, json, socket, datetime
-import util, fire, meta
-import pgbench, northwind
+import util, fire, meta, subprocess
+import pgbench, northwind, cluster
 
 cluster_dir = "cluster"
 
@@ -32,6 +32,22 @@ def validate_profile(profile):
         if profile==prof:
           found=True
   return found
+
+
+def call_pgedgecli(cmd, profile):
+  if validate_profile(profile):
+    os.system("~/go/bin/pgedge " + cmd  + " --profile=" + profile)
+  else:
+    util.exit_message(f"Profile needs to be registered")
+
+
+def get_pgedgecli(cmd, profile):
+  cluster_def={}
+  if validate_profile(profile):
+    cluster_def=subprocess.check_output("~/go/bin/pgedge " + cmd  + " --profile=" + profile, shell=True)
+  else:
+    util.exit_message(f"Profile needs to be registered")
+  return json.loads(cluster_def.decode('utf8'))
     
 
 def register(client_id, client_secret, profile="pgedge"):
@@ -55,35 +71,71 @@ def register(client_id, client_secret, profile="pgedge"):
 
 def list_clusters(profile="pgedge"):
   """List all clusters in a pgEdge Cloud Account"""
-  if validate_profile(profile):
-    os.system("~/go/bin/pgedge listclusters --profile=" + profile)
-  else:
-    util.exit_message(f"Profile needs to be registered")
+  call_pgedgecli("listclusters", profile)
+
+
+def list_cluster_nodes(cluster_id, profile="pgedge"):
+  """List all nodes in a pgEdge Cloud Account cluster"""
+  call_pgedgecli("listclusternodes " + cluster_id, profile)
   
 
-def import_cluster(cluster_name, cluster_id, client_id=None, client_secret=None):
+def import_cluster(cluster_id, profile="pgedge"):
   """Enable nodeCtl cluster commands on a pgEdge Cloud Cluster"""
-  util.exit_message("Coming Soon!")
+  cluster_def=get_pgedgecli("listcluster " + cluster_id, profile)
+  cluster_name=cluster_def[0]["name"]
+  db=cluster_def[0]["database"]["name"]
+  usr="pgedge"
+  passwd=""
+  pg=cluster_def[0]["database"]["pg_version"]
+  create_dt=cluster_def[0]["created_at"]
+  n=0
+  nodes=[]
+  paths=[]
+  ports=[]
+  ips=[]
+  for node in cluster_def[0]["node_groups"]["aws"]:
+    n=n+1
+    nodes.append(node["nodes"][0]["display_name"])
+    paths.append("/opt/pgedge")
+    ports.append("5432")
+    ips.append(node["cidr"].split('/')[0])
+  for node in cluster_def[0]["node_groups"]["azure"]:
+    n=n+1
+    nodes.append(node["nodes"][0]["display_name"])
+    paths.append("/opt/pgedge")
+    ports.append("5432")
+    ips.append(node["cidr"].split('/')[0])
+  for node in cluster_def[0]["node_groups"]["google"]:
+    n=n+1
+    nodes.append(node["nodes"][0]["display_name"])
+    paths.append("/opt/pgedge")
+    ports.append("5432")
+    ips.append(node["cidr"].split('/')[0])
+  cluster.create_remote_json(cluster_name, db, n, usr, passwd, pg, create_dt, nodes, paths, ports, ips)
+
 
 def push_metrics(cluster_name, target_info, client_id=None, client_secret=None):
   """push pgEdge Metrics to a specified target"""
   util.exit_message("Coming Soon!")
 
+
 def create_cluster(cluster_name, cluster_info, client_id=None, client_secret=None):
   """Create a pgEdge Cloud Cluster"""
   util.exit_message("Coming Soon!")
 
-def destroy_cluster(cluster_name, cluster_id, client_id=None, client_secret=None):
+
+def destroy_cluster(cluster_id, profile="pgedge"):
   """Delete a pgEdge Cloud Cluster"""
-  util.exit_message("Coming Soon!")
+  call_pgedgecli("deletecluster " + cluster_id, profile)
 
 
 if __name__ == '__main__':
   fire.Fire({
-    'register':        register,
-    'list-clusters':   list_clusters,
-    'import-cluster':  import_cluster,
-    'push-metrics':    push_metrics,
-    'create-cluster':  create_cluster,
-    'destroy-cluster': destroy_cluster
+    'register':           register,
+    'list-clusters':      list_clusters,
+    'list-cluster-nodes': list_cluster_nodes,
+    'import-cluster':     import_cluster,
+    'push-metrics':       push_metrics,
+    'create-cluster':     create_cluster,
+    'destroy-cluster':    destroy_cluster
   })
