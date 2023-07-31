4e9745c5766cf74696ce5beba79cc3f4ccf124b4
Susan Douglas <susan@pgedge.com>
2023-07-31 11:21:14 -0400

    Corrected paths, added variables in 315

--- t/315_um_install_hypopg.pl
+++ t/315_um_install_hypopg.pl
@@ -6,12 +6,16 @@ use strict;
 use warnings;
 
 use File::Which;
-#use PostgreSQL::Test::Cluster;
-#use PostgreSQL::Test::Utils;
-#use Test::More tests => 3;
 use IPC::Cmd qw(run);
 use Try::Tiny;
 use JSON;
+
+# Our parameters are:
+
+my $username = "lcusr";
+my $password = "password";
+my $database = "lcdb";
+my $version = "pg16";
 
 #
 # Move into the pgedge directory.
@@ -39,35 +43,46 @@ my $value = $success;
 # Then, we retrieve the port number from nodectl in json form... this is to catch cases where more than one copy of 
 # Postgres is running.
 #
-my $json = `./nc --json info pg16`;
+my $json = `./nc --json info`;
 print("my json = $json");
 my $out = decode_json($json);
+my $homedir = $out->[0]->{"homedir"};
 
-my $port = $out->[0]->{"port"};
+print("The home directory is {$homedir}\n"); 
+
+#
+# Then, we retrieve the port number from nodectl in json form... this is to catch cases where more than one copy of 
+# Postgres is running.
+#
+my $json2 = `./nc --json info pg16`;
+print("my json = $json2");
+my $out2 = decode_json($json2);
+my $port = $out2->[0]->{"port"};
+
 print("The port number is {$port}\n");
-#
-# Connect with psql, and confirm that I'm in the correct database.
-#
-
-my $cmd4 = qq(psql -t -h 127.0.0.1 -p $port -U admin -d demo -c "select * from current_database()");
-print("cmd4 = $cmd4\n");
-my($success4, $error_message4, $full_buf4, $stdout_buf4, $stderr_buf4)= IPC::Cmd::run(command => $cmd4, verbose => 0);
-
-print("success4 = $success4\n");
-print("stdout_buf4 = @$stdout_buf4\n");
 
 #
 # Then, we use the port number from the previous section to connect to psql and test for the existence of the extension.
 #
 
-my $cmd5 = qq(psql -t -h 127.0.0.1 -p $port -U admin -d demo -c "SELECT installed_version FROM pg_available_extensions WHERE name='hypopg'");
+my $cmd5 = qq($homedir/$version/bin/psql -t -h 127.0.0.1 -p $port -U username -d database -c "SELECT installed_version FROM pg_available_extensions WHERE name='hypopg'");
 print("cmd5 = $cmd5\n");
 my($success5, $error_message5, $full_buf5, $stdout_buf5, $stderr_buf5)= IPC::Cmd::run(command => $cmd5, verbose => 0);
 
 print("success5 = $success5\n");
 print("stdout_buf5 = @$stdout_buf5\n");
 
-if (defined($success5))
+#
+# Test
+#
+
+print("success5 = $success5\n");
+print("stdout_buf5 = @$full_buf5\n");
+print("If there is a version in @$full_buf5 we've installed hypopg!\n");
+
+my $substring = "1";
+if(index($stdout_buf5, $substring) == -1)
+
 {
     exit(0);
 }
