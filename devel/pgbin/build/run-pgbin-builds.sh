70de4decd00ea626924a020b3b1fd476df8361ad
Denis Lussier <denis@lussier.io>
2023-08-09 09:09:04 -0400

    tweak mulit builds script

--- devel/pgbin/build/run-pgbin-builds.sh
+++ devel/pgbin/build/run-pgbin-builds.sh
@@ -1,10 +1,14 @@
 
-if [ `uname` == "Linux" ]; then
-  sudo yum -y update
-fi
+#if [ `uname` == "Linux" ]; then
+#  sudo yum -y update
+#fi
+#
+#./sharedLibs.sh
 
-./sharedLibs.sh
-
+./build-all-pgbin.sh 11 -c
+./build-all-pgbin.sh 12 -c
+./build-all-pgbin.sh 13 -c
 ./build-all-pgbin.sh 14 -c
 ./build-all-pgbin.sh 15 -c
+#./build-all-pgbin.sh 16 -c
 
