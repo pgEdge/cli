02392c138b1a3aa01128edda2de0b405f9d4d9f8
Cady Motyka <cadymo@Cadys-MacBook-Pro.local>
2023-08-16 11:30:52 -0400

    Create and Delete Cluster

--- cli/scripts/secure.py
+++ cli/scripts/secure.py
@@ -25,7 +25,7 @@ def get_access_token(auth_json):
 
 
 def get_pgedge(cmd):
-  ## Call Get to pgEdge API
+  ## Call GET to pgEdge API
   with open(f"{cluster_dir}{os.sep}creds.json") as f:
     parsed_json = json.load(f)
     access=get_access_token(parsed_json)
@@ -36,7 +36,37 @@ def get_pgedge(cmd):
   if str(response.status_code)=='200':
     return response.json()
   else:
-    util.exit_message(f"Unable to run {cmd}")
+    util.exit_message(f"Unable to run get - {cmd}",1)
+
+
+def post_pgedge(cmd, data):
+  ## Call POST to pgEdge API
+  with open(f"{cluster_dir}{os.sep}creds.json") as f:
+    parsed_json = json.load(f)
+    access=get_access_token(parsed_json)
+  url = "https://api.pgedge.com/" + cmd 
+  header={}
+  header["Authorization"]="Bearer " + access
+  response = requests.post(url, headers=header, json=data)
+  if str(response.status_code)=='200':
+    return response.json()
+  else:
+    util.exit_message(f"Unable to run post - {cmd}",1)
+
+
+def delete_pgedge(cmd):
+  ## Call DELETE to pgEdge API
+  with open(f"{cluster_dir}{os.sep}creds.json") as f:
+    parsed_json = json.load(f)
+    access=get_access_token(parsed_json)
+  url = "https://api.pgedge.com/" + cmd 
+  header={}
+  header["Authorization"]="Bearer " + access
+  response = requests.delete(url, headers=header)
+  if str(response.status_code)=='200':
+    return response.json()
+  else:
+    util.exit_message(f"Unable to run delete - {cmd}",1)
 
 
 def login(client_id, client_secret):
@@ -53,29 +83,25 @@ def login(client_id, client_secret):
     access_token=get_access_token(auth_json)
     os.system(f"echo {access_token} > {cluster_dir}{os.sep}token.json") 
   except:
-    util.exit_message(f"Unable to create creds file")
-  print(f"Logged in to pgEdge Secure")
-  return 0
+    util.exit_message(f"Unable to create creds file",1)
+  util.exit_message(f"Logged in to pgEdge Secure",0)
 
 
 def list_clusters():
   """List all clusters in a pgEdge Cloud Account"""
   response=get_pgedge("clusters")
-  print(response)
-  return 0
+  print(json.dumps(response, indent=2))
 
 
 def list_cluster_nodes(cluster_id):
   """List all nodes in a pgEdge Cloud Account cluster"""
   response=get_pgedge(f"clusters/{cluster_id}/nodes")
-  print(response)
-  return 0
+  print(json.dumps(response, indent=2))
   
 
 def import_cluster(cluster_id):
   """Enable nodeCtl cluster commands on a pgEdge Cloud Cluster"""
   cluster_def=get_pgedge(f"clusters/{cluster_id}")
-  print(cluster_def)
   cluster_name=cluster_def["name"].lower()
   id=cluster_def["id"]
   db=cluster_def["database"]["name"]
@@ -96,8 +122,7 @@ def import_cluster(cluster_id):
     node_json["ip"]=node["public_ip_address"]
     nodes.append(node_json)
   cluster.create_remote_json(cluster_name, db, n, usr, passwd, pg, create_dt, id, nodes)
-  print("Cluster info json file created")
-  return 0
+  util.exit_message("Cluster info json file created",0)
 
 
 def get_cluster_id(cluster_name):
@@ -107,8 +132,8 @@ def get_cluster_id(cluster_name):
       parsed_json = json.load(f)
       cluster_id=parsed_json["id"]
   except:
-    util.exit_message(f"Cannot find cluster, you may need to import-cluster")
-  return cluster_id
+    util.exit_message(f"Cannot find cluster, you may need to import-cluster",1)
+  util.exit_message(cluster_id,0)
 
 
 def get_node_id(cluster_name, node_name):
@@ -120,8 +145,8 @@ def get_node_id(cluster_name, node_name):
         if n["name"]==node_name:
           node_id=n["id"]
   except:
-    util.exit_message(f"Cannot find node, you may need to import-cluster")
-  return node_id
+    util.exit_message(f"Cannot find node, you may need to import-cluster",1)
+  util.exit_message(node_id,0)
 
 
 def push_metrics(cluster_name, target_info, client_id=None, client_secret=None):
@@ -129,14 +154,22 @@ def push_metrics(cluster_name, target_info, client_id=None, client_secret=None):
   util.exit_message("Coming Soon!")
 
 
-def create_cluster(cluster_name, cluster_info, client_id=None, client_secret=None):
-  """Coming Soon: Create a pgEdge Cloud Cluster"""
-  util.exit_message("Coming Soon!")
+def create_cluster(cluster_name):
+  """Create a new Cloud Cluster based on json file"""
+  with open(f"{cluster_name}.json") as f:
+      parsed_json = json.load(f)
+  post_pgedge("clusters", parsed_json)
+  response=get_pgedge("clusters")
+  for cluster in response:
+    if cluster["name"]==cluster_name:
+      cluster_id=cluster["id"]
+  util.exit_message(f"Creating Cluster with id {cluster_id}",0)
 
 
 def destroy_cluster(cluster_id):
-  """Coming Soon: Delete a pgEdge Cloud Cluster"""
-  util.exit_message("Coming Soon!")
+  """Delete a pgEdge Cloud Cluster"""
+  response=delete_pgedge(f"clusters/{cluster_id}")
+  util.exit_message(f"Deleting Cluster with id {cluster_id}",0)
 
 
 if __name__ == '__main__':
