e464a2fbfac8409079405cde6d6c9bfb36e78a26
susan-pgedge <susan@pgedge.com>
2023-08-25 13:13:00 -0400

    Updating test paths

--- test/t/925_spock_repset-add-table_n1.pl
+++ test/t/925_spock_repset-add-table_n1.pl
@@ -19,8 +19,8 @@ my $version = "pg16";
 my $spock = "3.1";
 my $cluster = "demo";
 my $repset = "demo-repset";
-my $n1 = "~/work/nodectl/pgedge/cluster/demo/n1";
-my $n2 = "~/work/nodectl/pgedge/cluster/demo/n2";
+my $n1 = "~/work/nodectl/test/pgedge/cluster/demo/n1";
+my $n2 = "~/work/nodectl/test/pgedge/cluster/demo/n2";
 
 # On node 1:
 # ==========
