df698c2d57b56e796c71773c92c64b389835796d
susan-pgedge <susan@pgedge.com>
2023-08-25 15:49:40 -0400

    Updating schedule files/cases that create a 2 node cluster and tear it down again

--- test/t/999_gsg_breakdown.pl
+++ test/t/999_gsg_breakdown.pl
@@ -5,61 +5,60 @@
 
 use strict;
 use warnings;
-
 use File::Which;
-use File::Path;
 use IPC::Cmd qw(run);
 use Try::Tiny;
 use JSON;
+use lib './t/lib';
+use contains;
 
-#
+# Our parameters are:
+
+my $username = "lcusr";
+my $password = "password";
+my $database = "lcdb";
+my $version = "pg16";
+my $spock = "3.1";
+my $cluster = "demo";
+my $repset = "demo-repset";
+my $n1 = "~/work/nodectl/test/pgedge/cluster/demo/n1";
+my $n2 = "~/work/nodectl/test/pgedge/cluster/demo/n2";
+
+# We can retrieve the home directory from nodectl in json form... 
+my $json = `$n1/pgedge/nc --json info`;
+print("my json = $json");
+my $out = decode_json($json);
+my $homedir = $out->[0]->{"home"};
+print("The home directory is {$homedir}\n");
+
+# We can retrieve the port number from nodectl in json form...
+my $json2 = `$n1/pgedge/nc --json info pg16`;
+print("my json = $json2");
+my $out2 = decode_json($json2);
+my $datadir = $out2->[0]->{"datadir"};
+print("The data directory is {$datadir}\n");
+
 # Move into the pgedge directory
-#
 chdir ("./pgedge");
 
-#
-# Get the location of the home directory before removing pgEdge; store it in $home.
-#
-
-my $out = decode_json(`./nc --json info`);
-my $home = $out->[0]->{"home"};
-print("the home directory is = {$home}\n");
-
-my $out1 = decode_json(`./nc --json info pg16`);
-my $datadir = $out1->[0]->{"datadir"};
-print("the data directory is = {$datadir}\n");
-
-print("datadir = $datadir\n");
-print("home = $home\n");
-
-#
 # Then, use nodectl to remove the Postgres installation.
-#
 
-my $cmd = qq(./nodectl cluster destroy-local demo);
+my $cmd = qq($n1/pgedge/nodectl cluster destroy-local demo);
 print("cmd = $cmd\n");
 my ($success, $error_message, $full_buf, $stdout_buf, $stderr_buf)= IPC::Cmd::run(command => $cmd, verbose => 0);
 
-#
 # Diagnostic print statements
-#
 
-print("success = $success\n");
 print("full_buf = @$full_buf\n");
 print("stdout_buf = @$stdout_buf\n");
-print("stderr_buf = @$stderr_buf\n");
 
-#
 # Then, remove the data directory and the contents of the pgedge directory; then the pgedge directory is deleted.
-#
 
 File::Path::remove_tree($datadir);
 
-my $result = system("rm -rf $home");
+my $result = system("rm -rf $homedir");
 
-#
 # Then, remove the ~/.pgpass file.
-#
 
 my $cmd1 = qq(sudo rm ~/.pgpass);
 print("cmd1 = $cmd1");
