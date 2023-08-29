4787b994c3b47ec7dc32b3ae7d6a5aeece8933ad
susan-pgedge <susan@pgedge.com>
2023-08-29 10:53:48 -0400

    Lahari/Nalini - added t/906_check_node_one.pl

---
+++ test/t/906_check_node_one.pl
@@ -0,0 +1,81 @@
+# This is part of a complex test case; after creating a two node cluster on the localhost, 
+# the test case executes the commands in the Getting Started Guide at the pgEdge website.
+#
+# In this case, we'll check node 1 and check the repset on that node.
+# After creating the repset, we'll query the spock.replication_set_table to see if the repset exists. 
+
+
+use strict;
+use warnings;
+use File::Which;
+use IPC::Cmd qw(run);
+use Try::Tiny;
+use JSON;
+use lib './lib';
+use contains;
+
+# Our parameters are:
+
+my $cmd99 = qq(whoami);
+print("cmd99 = $cmd99\n");
+my ($success99, $error_message99, $full_buf99, $stdout_buf99, $stderr_buf99)= IPC::Cmd::run(command => $cmd99, verbose => 0);
+print("stdout_buf99 = @$stdout_buf99\n");
+
+my $repuser = "@$stdout_buf99[0]";
+my $username = "lcusr";
+my $password = "password";
+my $database = "lcdb";
+my $version = "pg16";
+my $spock = "3.1";
+my $cluster = "demo";
+my $repset = "demo-repset";
+my $n1 = "~/work/nodectl/test/pgedge/cluster/demo/n1";
+my $n2 = "~/work/nodectl/test/pgedge/cluster/demo/n2";
+
+# We can retrieve the home directory from nodectl in json form... 
+
+my $json = `$n1/pgedge/nc --json info`;
+print("my json = $json");
+my $out = decode_json($json);
+my $homedir = $out->[0]->{"home"};
+print("The home directory is {$homedir}\n"); 
+
+# We can retrieve the port number from nodectl in json form...
+my $json2 = `$n1/pgedge/nc --json info pg16`;
+print("my json = $json2");
+my $out2 = decode_json($json2);
+my $port = $out2->[0]->{"port"};
+print("The port number is {$port}\n");
+
+# check node 1 and the repset entry on n1: 
+
+my $cmd2 = qq($homedir/nodectl spock node-list $database);
+print("cmd2 = $cmd2\n");
+my ($success2, $error_message2, $full_buf2, $stdout_buf2, $stderr_buf2)= IPC::Cmd::run(command => $cmd2, verbose => 0);
+
+print("We just invoked the ./nc spock node-list n1 command\n");
+
+# Then, use the info to connect to psql and test for the existence of the replication set.
+
+my $cmd5 = qq($homedir/$version/bin/psql -t -h 127.0.0.1 -p $port -d $database -c "SELECT * FROM spock.replication_set");
+print("cmd5 = $cmd5\n");
+my($success5, $error_message5, $full_buf5, $stdout_buf5, $stderr_buf5)= IPC::Cmd::run(command => $cmd5, verbose => 0);
+
+# Test to confirm that cluster is set up.
+
+print("We just installed pgedge/spock in $n1.\n");
+
+if(contains(@$stdout_buf5[0], "demo-repset"))
+
+{
+    exit(0);
+}
+else
+{
+    exit(1);
+}
+
+
+
+
+
