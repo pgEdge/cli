bc4d4765e519bc683b974f43e2b7a91916525ad8
susan-pgedge <susan@pgedge.com>
2023-08-30 16:34:52 -0400

    Updated success/failure; still need cleanup script

--- test/t/4050_spock_node_create_error.pl
+++ test/t/4050_spock_node_create_error.pl
@@ -9,56 +9,31 @@ use File::Which;
 use IPC::Cmd qw(run);
 use Try::Tiny;
 use JSON;
+use lib './t/lib';
+use contains;
 
-
-#
-# First, we use nodectl to create a two-node cluster named demo; the nodes are named n1/n2 (default names), 
-# the database is named lcdb (default), and it is owned by lcdb (default).
-# 
-
-my $cmd = qq(./pgedge/nodectl cluster create-local demo 2 --pg 16);
-print("cmd = $cmd\n");
-my ($success, $error_message, $full_buf, $stdout_buf, $stderr_buf)= IPC::Cmd::run(command => $cmd, verbose => 0);
-
-print("full_buf = @$full_buf\n");
-print("stderr_buf = @$stderr_buf\n");
-print("This s/b a 2 node cluster named demo, owned by lcdb, with a db named lcdb.  The nodes are named n1/n2.\n");
-print("This test assumes they're running on 6432 and 6433\n");
 
 #
 # Call spock node-create without a node name: 
 #
 
-my $cmd2 = qq(./pgedge/nodectl spock node-create 'host=127.0.0.1 port=6432 user=lcdb dbname=lcdb' lcdb);
+my $cmd2 = qq(./nodectl spock node-create 'host=127.0.0.1 port=6432 user=lcdb dbname=lcdb' lcdb);
 print("cmd2 = $cmd2\n");
 my ($success2, $error_message2, $full_buf2, $stdout_buf2, $stderr_buf2)= IPC::Cmd::run(command => $cmd2, verbose => 0);
 
 print("We just invoked the ./nc spock node-create n1 command but without a node name.\n");
-
-#
-# Print statements
-#
-
 print("error_message2 = $error_message2\n");
-print("full_buf2 = @$full_buf2\n");
-print("stdout_buf2 = @$stdout_buf2\n");
-print("stderr_buf2 = @$stderr_buf2\n");
-
-#
-print("If the server reports an ERROR for the missing node name, the test succeeded\n");
 #
 
-my $substring = "ERROR";
-if (index($stdout_buf2, $substring) == -1)
+if(contains($error_message2, "exited"))
 
 {
-  exit(0);
+    exit(0);
 }
 else
 {
- exit(1);
+    exit(1);
 }
-
 
 
 
--- test/t/4120_spock_repset_create_error.pl
+++ test/t/4120_spock_repset_create_error.pl
@@ -8,54 +8,28 @@ use File::Which;
 use IPC::Cmd qw(run);
 use Try::Tiny;
 use JSON;
+use lib './t/lib';
+use contains;
 
-#
-# Create a variable with the path to each node.
-# 
 
-my $n1 = "~/work/nodectl/pgedge/cluster/demo/n1";
+# Call the command to create a repset, but provide the wrong database name: 
 
-# Call the command to create a repset, but provide the wronge database name: 
-#
-
-my $cmd3 = qq($n1/nodectl spock repset-create my_repset postgres);
+my $cmd3 = qq(./nodectl spock repset-create my_repset postgres);
 print("cmd3 = $cmd3\n");
 my ($success3, $error_message3, $full_buf3, $stdout_buf3, $stderr_buf3)= IPC::Cmd::run(command => $cmd3, verbose => 0);
 
 print("error_message3 = $error_message3\n");
-print("full_buf3 = @$full_buf3\n");
 print("stdout_buf3 = @$stdout_buf3\n");
-print("stderr_buf3 = @$stderr_buf3\n");
 
-#
-print("If the server reports an ERROR for the missing database name, the test succeeded\n");
-#
 
-my $substring = "ERROR";
-if (index($stdout_buf3, $substring) == -1)
+print("If the server reports an ERROR for the wrong database name, the test succeeded\n");
 
+if(contains($error_message3, "failed"))
 {
-  exit(0);
+    exit(0);
 }
 else
 {
- exit(1);
+    exit(1);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
