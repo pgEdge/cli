d86b0ebc92917f490b14edf957d559caae3e4db2
Denis Lussier <denis@lussier.io>
2023-09-20 15:55:14 -0400

    wip for d11 & a17

--- devel/pgbin/build/run-pgbin-builds.sh
+++ devel/pgbin/build/run-pgbin-builds.sh
@@ -1,14 +1,8 @@
 
-#if [ `uname` == "Linux" ]; then
-#  sudo yum -y update
-#fi
-#
-#./sharedLibs.sh
-
-./build-all-pgbin.sh 11 -c
 ./build-all-pgbin.sh 12 -c
 ./build-all-pgbin.sh 13 -c
 ./build-all-pgbin.sh 14 -c
 ./build-all-pgbin.sh 15 -c
 #./build-all-pgbin.sh 16 -c
+#./build-all-pgbin.sh 17 -c
 
