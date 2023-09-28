d9ec33c82ec3334658152cd9fd84a954394fb67c
Denis Lussier <denis@lussier.io>
2023-09-28 14:56:22 +0000

    dev tweak for build/test

--- devel/stopHTTP.sh
+++ devel/stopHTTP.sh
@@ -3,6 +3,7 @@ pid=`ps aux | grep [h]ttp.server | awk '{print $2}'`
 if [ ! "$pid" == "" ]; then
   echo "killing ($pid)"
   kill -9 $pid > /dev/null 2>&1
+  sleep 1
 else
   echo "http.server not running"
 fi
