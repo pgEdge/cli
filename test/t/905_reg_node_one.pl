753dfa91da0d1b6881095b9757ac368bf98d74ed
susan-pgedge <susan@pgedge.com>
2023-10-05 13:42:22 -0400

    Updated tests in create_two_node_cluster schedule to check for errors that occur for missing parameters from repset-create stmt; now in t/905

--- test/t/905_reg_node_one.pl
+++ test/t/905_reg_node_one.pl
@@ -17,7 +17,6 @@ use contains;
 # Our parameters are:
 
 my $cmd99 = qq(whoami);
-print("cmd99 = $cmd99\n");
 my ($success99, $error_message99, $full_buf99, $stdout_buf99, $stderr_buf99)= IPC::Cmd::run(command => $cmd99, verbose => 0);
 print("stdout_buf99 = @$stdout_buf99\n");
 
@@ -56,12 +55,32 @@ my ($success2, $error_message2, $full_buf2, $stdout_buf2, $stderr_buf2)= IPC::Cm
 
 print("stdout_buf2 = @$stdout_buf2\n");
 
+# The next couple of stanzas test to make sure we ERROR out sensibly if a parameter is omitted...
+# Test to ensure that a repset name must be provided:
+
+my $cmd30 = qq($homedir/nodectl spock repset-create $database);
+print("cmd30 = $cmd30\n");
+my ($success30, $error_message30, $full_buf30, $stdout_buf30, $stderr_buf30)= IPC::Cmd::run(command => $cmd30, verbose => 0);
+
+if(!(contains(@$stderr_buf30[0], "ERROR")))
+{
+    exit(1);
+}
+
+# Test to ensure that a database name must be provided:
+
+my $cmd31 = qq($homedir/nodectl spock repset-create $database);
+print("cmd31 = $cmd31\n");
+my ($success31, $error_message31, $full_buf31, $stdout_buf31, $stderr_buf31)= IPC::Cmd::run(command => $cmd31, verbose => 0);
+
+if(!(contains(@$stderr_buf31[0], "ERROR")))
+{
+    exit(1);
+}
+
 my $cmd3 = qq($homedir/nodectl spock repset-create $repset $database);
 print("cmd3 = $cmd3\n");
 my ($success3, $error_message3, $full_buf3, $stdout_buf3, $stderr_buf3)= IPC::Cmd::run(command => $cmd3, verbose => 0);
-
-print("success3 = $success3\n");
-print("stdout_buf3 = @$stdout_buf3\n");
 
 print("We just executed the command that creates the replication set (demo-repset)\n");
 
