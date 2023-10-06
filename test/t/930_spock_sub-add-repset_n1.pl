4dd3db6052dee221234521cb11db627df5bcb205
susan-pgedge <susan@pgedge.com>
2023-10-06 08:30:18 -0400

    Updated t/930_spock_sub-add-repset_n1.pl to add checks for missing params

--- test/t/930_spock_sub-add-repset_n1.pl
+++ test/t/930_spock_sub-add-repset_n1.pl
@@ -38,14 +38,44 @@ my $out2 = decode_json($json2);
 my $port = $out2->[0]->{"port"};
 print("The port number is {$port}\n");
 
+# Confirm that the spock sub-add-repset command fails gracefully if the subscription name is omitted:
+
+my $cmd2 = qq($homedir/nodectl spock sub-add-repset $repset $database);
+print("cmd2 = $cmd2\n");
+my($success2, $error_message2, $full_buf2, $stdout_buf2, $stderr_buf2)= IPC::Cmd::run(command => $cmd2, verbose => 0);
+
+if(!(contains(@$stderr_buf2[0], "ERROR")))
+{
+    exit(1);
+}
+
+# Confirm that the spock sub-add-repset command fails gracefully if the repset name is omitted:
+
+my $cmd3 = qq($homedir/nodectl spock sub-add-repset sub_n1n2 $database);
+print("cmd3 = $cmd3\n");
+my($success3, $error_message3, $full_buf3, $stdout_buf3, $stderr_buf3)= IPC::Cmd::run(command => $cmd3, verbose => 0);
+
+if(!(contains(@$stderr_buf3[0], "ERROR")))
+{
+    exit(1);
+}
+
+# Confirm that the spock sub-add-repset command fails gracefully if the database name is omitted:
+
+my $cmd4 = qq($homedir/nodectl spock sub-add-repset sub_n1n2 $repset);
+print("cmd4 = $cmd4\n");
+my($success4, $error_message4, $full_buf4, $stdout_buf4, $stderr_buf4)= IPC::Cmd::run(command => $cmd4, verbose => 0);
+
+if(!(contains(@$stderr_buf4[0], "ERROR")))
+{
+    exit(1);
+}
+
 # Then, add the repset to the subscription on node 1:
 
 my $cmd23 = qq($homedir/nodectl spock sub-add-repset sub_n1n2 $repset $database);
 print("cmd23 = $cmd23\n");
 my($success23, $error_message23, $full_buf23, $stdout_buf23, $stderr_buf23)= IPC::Cmd::run(command => $cmd23, verbose => 0);
-
-print("success23 = $success23\n");
-print("stdout_buf23 = @$stdout_buf23\n");
 
 print("We just added the replication set to the subscription on ($n1) to connect to ($n2).\n");
 
