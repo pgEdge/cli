ae09ebd145782102fee08f7be9ca44b39810b868
Denis Lussier <denis@lussier.io>
2023-10-16 23:31:37 +0000

    2nd pass at snowflake

---
+++ src/snowflake-pgXX/remove-snowflake-pgXX.py
@@ -0,0 +1,9 @@
+ 
+###############################################################
+######          Copyright (c)  2023 PGEDGE           ##########
+###############################################################
+
+import util
+
+util.remove_pgconf_keyval("pgXX", "shared_preload_libraries", "snowflake")
+
