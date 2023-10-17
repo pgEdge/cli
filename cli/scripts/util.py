843844cd4cf2edb5391baaafba16357b7c549b51
Denis Lussier <denis@lussier.io>
2023-10-17 13:41:22 +0000

    open up 24.002 for dev

--- cli/scripts/util.py
+++ cli/scripts/util.py
@@ -2,7 +2,7 @@
 #  Copyright 2022-2024 PGEDGE  All rights reserved. #
 #####################################################
 
-MY_VERSION = "24.001"
+MY_VERSION = "24.002"
 
 from subprocess import Popen, PIPE, STDOUT
 from datetime import datetime, timedelta
--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 
 bundle=pgedge
 api=nodectl
-hubV=24.001
+hubV=24.002
 
 spock32V=3.2dev3-1
 spock31V=3.1.7-1
