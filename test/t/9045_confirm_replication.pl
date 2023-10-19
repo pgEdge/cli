c4bb807d2b09cd60c3b54c3b34c9c5433966c1be
susan-pgedge <susan@pgedge.com>
2023-10-19 09:37:54 -0400

    Updated error message for pg parameter (t/602)

--- test/t/9045_confirm_replication.pl
+++ test/t/9045_confirm_replication.pl
@@ -24,23 +24,46 @@ my $repset = "demo-repset";
 my $n1 = "~/work/nodectl/test/pgedge/cluster/demo/n1";
 my $n2 = "~/work/nodectl/test/pgedge/cluster/demo/n2";
 
-# We can retrieve the home directory from nodectl in json form...
-my $json = `$n2/pgedge/nc --json info`;
+# We can retrieve the home directory from nodectl in json form... 
+my $json = `$n1/pgedge/nc --json info`;
 #print("my json = $json");
 my $out = decode_json($json);
 my $homedir = $out->[0]->{"home"};
-print("The home directory is {$homedir}\n");
+print("The home directory of n1 is {$homedir}\n");
 
 # We can retrieve the port number from nodectl in json form...
-my $json2 = `$n2/pgedge/nc --json info pg16`;
+my $json2 = `$n1/pgedge/nc --json info pg16`;
 #print("my json = $json2");
 my $out2 = decode_json($json2);
 my $port = $out2->[0]->{"port"};
-print("The port number is {$port}\n");
+print("The port number of n1 is {$port}\n");
+
+# We can retrieve the home directory from nodectl in json form... 
+my $json3 = `$n2/pgedge/nc --json info`;
+#print("my json = $json3");
+my $out3 = decode_json($json3);
+my $homedir2 = $out3->[0]->{"home"};
+print("The home directory of n2 is {$homedir2}\n");
+
+# We can retrieve the port number from nodectl in json form...
+my $json4 = `$n2/pgedge/nc --json info pg16`;
+#print("my json = $json4");
+my $out4 = decode_json($json4);
+my $port2 = $out4->[0]->{"port"};
+print("The port number of n2 is {$port2}\n");
+
+# Connect to psql on node 1 and add a record to 'foo':
+#
+
+my $cmd28 = qq($homedir/$version/bin/psql -t -h 127.0.0.1 -p $port -d $database -c "INSERT INTO pgbench_tellers VALUES (78, 89, 90, 'test')");
+print("cmd28 = $cmd28\n");
+my($success28, $error_message28, $full_buf28, $stdout_buf28, $stderr_buf28)= IPC::Cmd::run(command => $cmd28, verbose => 0);
+print("stdout_buf = @$stdout_buf28\n");
+print("We've just added a record to the pgbench_tellers table on $n1 using $port\n");
 
 # Test to see if replication is working on node 2
 
-my $cmd29 = qq($homedir/$version/bin/psql -t -h 127.0.0.1 -p $port -d $database -c "SELECT * FROM pgbench_tellers");
+my $cmd29 = qq($homedir2/$version/bin/psql -t -h 127.0.0.1 -p $port2 -d $database -c "SELECT * FROM pgbench_tellers");
 print("cmd29 = $cmd29\n");
 my($success29, $error_message29, $full_buf29, $stdout_buf29, $stderr_buf29)= IPC::Cmd::run(command => $cmd29, verbose => 0);
 print("stdout_buf = @$stdout_buf29\n");
