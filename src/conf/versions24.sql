2d2b4e83eb6281a17d7ca1d35fe7ce2f5e0cc9da
Denis Lussier <denis@lussier.io>
2023-10-20 12:41:17 +0000

    getting ready for 24.002 to release later today

--- src/conf/versions24.sql
+++ src/conf/versions24.sql
@@ -85,7 +85,7 @@ INSERT INTO categories VALUES (9,  87, 'Management & Monitoring', 'Manage/Monito
 -- ## HUB ################################
 INSERT INTO projects VALUES ('hub', 'app', 0, 0, 'hub', 0, 'https://github.com/pgedge/nodectl','',0,'','','');
 INSERT INTO releases VALUES ('hub', 1, 'hub', '', '', 'hidden', '', 1, '', '', '');
-INSERT INTO versions VALUES ('hub', '24.002', '',  1, '20231024', '', '', '');
+INSERT INTO versions VALUES ('hub', '24.002', '',  1, '20231020', '', '', '');
 INSERT INTO versions VALUES ('hub', '24.001', '',  0, '20231017', '', '', '');
 INSERT INTO versions VALUES ('hub', '23.134', '',  0, '20231013', '', '', '');
 
@@ -110,15 +110,18 @@ INSERT INTO versions VALUES ('pg14', '14.8-1', 'el8', 0, '20230511','', '', '');
 
 INSERT INTO releases VALUES ('pg15', 2, 'pg', '', '', 'prod', 
   '<font size=-1>New in <a href=https://www.postgresql.org/docs/15/release-15.html>2022</a></font>', 1, 'POSTGRES', '', '');
-INSERT INTO versions VALUES ('pg15', '15.4-2',  'el8, el9, arm9',      1, '20230829','', '', '');
+INSERT INTO versions VALUES ('pg15', '15.4-3',  'el9',                 1, '20231020','', '', '');
+INSERT INTO versions VALUES ('pg15', '15.4-2',  'el8, el9, arm9',      0, '20230829','', '', '');
 
 INSERT INTO releases VALUES ('pg16', 2, 'pg', '', '', 'prod', 
   '<font size=-1>New in <a href=https://www.postgresql.org/docs/16/release-16.html>2023!</a></font>', 1, 'POSTGRES', '', '');
-INSERT INTO versions VALUES ('pg16', '16.0-1',    'el9, arm9', 1, '20230914','', '', '');
+INSERT INTO versions VALUES ('pg16', '16.0-2',    'el9',       1, '20231020','', '', '');
+INSERT INTO versions VALUES ('pg16', '16.0-1',    'el9, arm9', 0, '20230914','', '', '');
 
 INSERT INTO releases VALUES ('pg17', 2, 'pg', '', '', 'prod', 
   '<font size=-1>New in <a href=https://www.postgresql.org/docs/17/release-17.html>2024!</a></font>', 1, 'POSTGRES', '', '');
-INSERT INTO versions VALUES ('pg17', '17devel-1', 'el9', 1, '20230927','', '', '');
+INSERT INTO versions VALUES ('pg17', '17devel-2', 'el9', 1, '20231020','', '', '');
+INSERT INTO versions VALUES ('pg17', '17devel-1', 'el9', 0, '20230927','', '', '');
 
 INSERT INTO projects VALUES ('orafce', 'ext', 4, 0, 'hub', 0, 'https://github.com/orafce/orafce/releases',
   'orafce', 1, 'larry.png', 'Ora Built-in Packages', 'https://github.com/orafce/orafce#orafce---oracles-compatibility-functions-and-packages');
