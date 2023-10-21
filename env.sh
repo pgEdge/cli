3bb639270f4e8bd103beab0c4715aa217fa7ed3a
Denis Lussier <denis@lussier.io>
2023-10-21 16:49:21 +0000

    open up 23.003 for dev

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 
 bundle=pgedge
 api=nodectl
-hubV=24.002
+hubV=24.003
 
 spock32V=3.2dev5-1
 spock31V=3.1.7-1
--- src/conf/versions24.sql
+++ src/conf/versions24.sql
@@ -85,7 +85,8 @@ INSERT INTO categories VALUES (9,  87, 'Management & Monitoring', 'Manage/Monito
 -- ## HUB ################################
 INSERT INTO projects VALUES ('hub', 'app', 0, 0, 'hub', 0, 'https://github.com/pgedge/nodectl','',0,'','','');
 INSERT INTO releases VALUES ('hub', 1, 'hub', '', '', 'hidden', '', 1, '', '', '');
-INSERT INTO versions VALUES ('hub', '24.002', '',  1, '20231021', '', '', '');
+INSERT INTO versions VALUES ('hub', '24.003', '',  1, '20231028', '', '', '');
+INSERT INTO versions VALUES ('hub', '24.002', '',  0, '20231021', '', '', '');
 INSERT INTO versions VALUES ('hub', '24.001', '',  0, '20231017', '', '', '');
 INSERT INTO versions VALUES ('hub', '23.134', '',  0, '20231013', '', '', '');
 
