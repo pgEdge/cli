cb2ee915390e6bb0af3ec67fae030334a1dda1d9
susan-pgedge <susan@pgedge.com>
2023-10-23 15:58:39 -0400

    Updating test scripts for v17/Spock 3.2

--- test/t/9500_check_IN_UP_TR_DEL.pl
+++ test/t/9500_check_IN_UP_TR_DEL.pl
@@ -1,5 +1,5 @@
-# This test case updates a record on node 1 that should then be written to node 2 
-# in test case 915.
+# This script confirms that I/U/D/T statements work as expected in a default installation.
+
 
 use strict;
 use warnings;
@@ -15,8 +15,8 @@ use contains;
 my $username = "lcusr";
 my $password = "password";
 my $database = "lcdb";
-my $version = "pg16";
-my $spock = "3.1";
+my $version = "pg17";
+my $spock = "3.2";
 my $cluster = "demo";
 my $repset = "demo-repset";
 my $n1 = "~/work/nodectl/test/pgedge/cluster/demo/n1";
@@ -30,7 +30,7 @@ my $homedir = $out->[0]->{"home"};
 print("The home directory is {$homedir}\n");
 
 # We can retrieve the port number from nodectl in json form...
-my $json2 = `$n1/pgedge/nc --json info pg16`;
+my $json2 = `$n1/pgedge/nc --json info $version`;
 #print("my json = $json2");
 my $out2 = decode_json($json2);
 my $port = $out2->[0]->{"port"};
