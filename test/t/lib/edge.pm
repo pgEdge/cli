b9a11e2ca7af209fafcc9a4f05be97a56509f7fa
susan-pgedge <susan@pgedge.com>
2023-10-27 15:14:14 -0400

    Updating files/sample file with variables for use with perl test scripts

--- test/t/lib/edge.pm
+++ test/t/lib/edge.pm
@@ -4,16 +4,16 @@ package edge;
 
 our $username = $ENV{EDGE_USERNAME};
 our $password = $ENV{EDGE_PASSWORD};
-our $database = $ENV{EDGE_DBNAME};
+our $database = $ENV{EDGE_DB};
 our $inst_version = $ENV{EDGE_INST_VERSION};
-our $version = $ENV{EDGE_CMD_VERSION};
-our $spock = $ENV{EDGE_SPOCK_VERSION};
-our $cluster = $ENV{EDGE_CLUSTER_NAME};
-our $repset = $ENV{EDGE_REPSET_NAME};
-our $n1 = $ENV{EDGE_PATH_TO_N1};
-our $n2 = $ENV{EDGE_PATH_TO_N2};
-our $n3 = $ENV{EDGE_PATH_TO_N3};
-
+our $version = $ENV{EDGE_VERSION};
+our $spock = $ENV{EDGE_SPOCK};
+our $cluster = $ENV{EDGE_CLUSTER};
+our $repset = $ENV{EDGE_REPSET};
+our $n1 = $ENV{EDGE_N1};
+our $n2 = $ENV{EDGE_N2};
+our $n3 = $ENV{EDGE_N3};
+our $nodes = $ENV{NODES};
 
 # Leave the 1 on the end of this file:
 
