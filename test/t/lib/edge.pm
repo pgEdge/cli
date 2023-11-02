6f033257a82b67e30e39177dca06a600406a5936
susan-pgedge <susan@pgedge.com>
2023-11-02 11:39:51 -0400

    Updating variable files for testcases

--- test/t/lib/edge.pm
+++ test/t/lib/edge.pm
@@ -2,6 +2,8 @@
 
 package edge;
 
+our $repo = $ENV{EDGE_REPO};
+our $host = $ENV{EDGE_HOST};
 our $username = $ENV{EDGE_USERNAME};
 our $password = $ENV{EDGE_PASSWORD};
 our $database = $ENV{EDGE_DB};
@@ -13,7 +15,20 @@ our $repset = $ENV{EDGE_REPSET};
 our $n1 = $ENV{EDGE_N1};
 our $n2 = $ENV{EDGE_N2};
 our $n3 = $ENV{EDGE_N3};
-our $nodes = $ENV{NODES};
+our $nodes = $ENV{EDGE_NODES};
+our $port1 = $ENV{EDGE_PORT1};
+our $port2 = $ENV{EDGE_PORT2};
+our $homedir1 = $ENV{EDGE_HOMEDIR1};
+our $homedir2 = $ENV{EDGE_HOMEDIR2};
+our $chdir1 =  $ENV{EDGE_CHDIR1};
+our $chdir2 =  $ENV{EDGE_CHDIR2};
+our $node1_name = $ENV{EDGE_NODE1_NAME};
+our $node2_name = $ENV{EDGE_NODE2_NAME};
+our $repset = $ENV{EDGE_REPSET};
+our $set_name = $ENV{EDGE_SETNAME};
+our $schema = $ENV{EDGE_SCHEMA};
+our $table_name = $ENV{EDGE_TABLE};
+
 
 # Leave the 1 on the end of this file:
 
