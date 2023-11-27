9853bfb905ed55e95dc845a9ddd35bec46e128d2
Lahari Giddi <lahari@pgedge.com>
2023-11-27 18:39:45 +0530

    flake8

--- cli/scripts/api.py
+++ cli/scripts/api.py
@@ -3,7 +3,6 @@
 #####################################################
 
 import json, os, platform, subprocess, sys, time
-import util
 from datetime import datetime, timedelta
 from operator import itemgetter
 
@@ -12,22 +11,22 @@ isPy3 = True
 try:
     from colorama import init
     init()
-except ImportError:
+except ImportError as e:
     pass
 
 scripts_lib_path = os.path.join(os.path.dirname(__file__), 'lib')
 this_platform_system = str(platform.system())
 platform_lib_path = os.path.join(scripts_lib_path, this_platform_system)
 if os.path.exists(platform_lib_path):
-    if platform_lib_path not in sys.path:
-        sys.path.append(platform_lib_path)
+  if platform_lib_path not in sys.path:
+    sys.path.append(platform_lib_path)
+
+import util
 
 python_exe = sys.executable
 python_ver = platform.python_version()
 
-
 class bcolors:
-
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -39,7 +38,6 @@ class bcolors:
     BACKGROUND = '\033[42m'
     ITALIC = '\033[3m'
 
-
 bold_start = bcolors.BOLD
 bold_end = bcolors.ENDC
 italic_start = bcolors.ITALIC
@@ -49,45 +47,44 @@ error_start = bcolors.FAIL
 
 
 def format_help(p_input):
-    inp = str(p_input)
-    inp_lst = inp.split()
+  inp = str(p_input)
+  inp_lst = inp.split()
 
-    p_1st = None
-    if inp_lst:
-        p_1st = str(inp_lst[0])
+  p_1st = None
+  if inp_lst:
+    p_1st = str(inp_lst[0])
 
-    if p_1st in ("#", "##", "###"):
-        skip_len = len(p_1st) + 1
-        inp = inp[skip_len:]
-        inp = inp.replace("`", "")
-        inp = bold_start + str(inp.upper()) + bold_end
+  if p_1st in ("#", "##", "###"):
+    skip_len = len(p_1st) + 1
+    inp = inp[skip_len:]
+    inp = inp.replace("`", "")
+    inp = bold_start + str(inp.upper()) + bold_end
 
-    elif inp == "```":
-        return(None)
+  elif inp == "```":
+    return(None)
 
-    else:
-        inp = inp.replace(" # ", italic_start + " # ")
-        inp = "  " + inp + " " + italic_end
+  else:
+    inp = inp.replace(" # ", italic_start + " # ")
+    inp = "  " + inp + " " + italic_end
 
-    return(inp)
+  return(inp)
 
 
 def get_pip_ver():
-    try:
-        import pip
-        return(pip.__version__)
-    except ImportError:
-        pass
-    return("None")
+  try:
+    import pip
+    return(pip.__version__)
+  except ImportError as e:
+    pass
+  return("None")
 
 
-def cli_unicode(p_str, p_encoding, errors="ignore"):
-    return str(p_str)
-
+def cli_unicode(p_str,p_encoding,errors="ignore"):
+  return str(p_str)
 
 try:
     test_unicode = unicode("test")
-except NameError:
+except NameError as e:
     unicode = cli_unicode
 
 
@@ -102,81 +99,80 @@ def check_output_wmic (p_cmds):
 
 
 def top(display=True, isJson=False):
+  try:
+    import psutil
+  except ImportError as e:
+    util.exit_message("Missing psutil module", 1)
+
+  current_timestamp = int(time.mktime(datetime.utcnow().timetuple()))
+  jsonDict = {}
+  procs = []
+  for p in psutil.process_iter():
     try:
-        import psutil
-    except ImportError:
-        util.exit_message("Missing psutil module", 1)
+      p = p.as_dict(attrs=
+        ['pid', 'username', 'cpu_percent', 'memory_percent', 'cpu_times', 'name'])
+    except (psutil.NoSuchProcess, IOError, OSError) as e:
+      pass
+    else:
+      procs.append(p)
 
-    current_timestamp = int(time.mktime(datetime.utcnow().timetuple()))
-    jsonDict = {}
-    procs = []
-    for p in psutil.process_iter():
-        try:
-            p = p.as_dict(attrs=['pid', 'username', 'cpu_percent', 'memory_percent', 'cpu_times', 'name'])
-        except (psutil.NoSuchProcess, IOError, OSError):
-            pass
-        else:
-            procs.append(p)
+  if not display:
+    return
 
-    if not display:
-        return
+  processes = sorted(procs, key=lambda p: p['cpu_percent'], reverse=True)
 
-    processes = sorted(procs, key=lambda p: p['cpu_percent'], reverse=True)
+  network_usage = psutil.net_io_counters()
+  jsonDict['kb_sent'] = network_usage.bytes_sent / 1024
+  jsonDict['kb_recv'] = network_usage.bytes_recv / 1024
 
-    network_usage = psutil.net_io_counters()
-    jsonDict['kb_sent'] = network_usage.bytes_sent / 1024
-    jsonDict['kb_recv'] = network_usage.bytes_recv / 1024
+  cpu = psutil.cpu_times_percent(percpu=False)
+  iowait = ""
+  if util.get_platform() == "Linux":
+    jsonDict['iowait'] = str(cpu.iowait)
+    iowait = "," + str(cpu.iowait).rjust(5) + "%wa"
 
-    cpu = psutil.cpu_times_percent(percpu=False)
-    iowait = ""
-    if util.get_platform() == "Linux":
-        jsonDict['iowait'] = str(cpu.iowait)
-        iowait = "," + str(cpu.iowait).rjust(5) + "%wa"
+  jsonDict['current_timestamp'] = current_timestamp
+  jsonDict['cpu_user'] = str(cpu.user)
+  jsonDict['cpu_system'] = str(cpu.system)
+  jsonDict['cpu_idle'] = str(cpu.idle)
+  if not isJson:
+    print("CPU(s):" + str(cpu.user).rjust(5) + "%us," + \
+      str(cpu.system).rjust(5) + "%sy," + str(cpu.idle).rjust(5) + "%id" + iowait)
 
-    jsonDict['current_timestamp'] = current_timestamp
-    jsonDict['cpu_user'] = str(cpu.user)
-    jsonDict['cpu_system'] = str(cpu.system)
-    jsonDict['cpu_idle'] = str(cpu.idle)
-    if not isJson:
-        print(
-                "CPU(s):" + str(cpu.user).rjust(5) + "%us," +
-                str(cpu.system).rjust(5) + "%sy," + str(cpu.idle).rjust(5) + "%id" + iowait
-        )
+  disk = psutil.disk_io_counters(perdisk=False)
+  read_kb = disk.read_bytes / 1024
+  write_kb = disk.write_bytes / 1024
+  jsonDict['kb_read']  = str(read_kb)
+  jsonDict['kb_write']  = str(write_kb)
+  if not isJson:
+    print("DISK: kB_read " + str(read_kb) + ", kB_written " + str(write_kb))
 
-    disk = psutil.disk_io_counters(perdisk=False)
-    read_kb = disk.read_bytes / 1024
-    write_kb = disk.write_bytes / 1024
-    jsonDict['kb_read'] = str(read_kb)
-    jsonDict['kb_write'] = str(write_kb)
-    if not isJson:
-        print("DISK: kB_read " + str(read_kb) + ", kB_written " + str(write_kb))
+  uptime = datetime.now() - datetime.fromtimestamp(psutil.boot_time())
+  str_uptime = str(uptime).split('.')[0]
+  line = ""
+  uname_len = 8
+  av1, av2, av3 = os.getloadavg()
+  str_loadavg = "%.2f %.2f %.2f  " % (av1, av2, av3)
+  line = bold_start + "Load average: " + bold_end + str_loadavg
+  jsonDict['load_avg']  = str(str_loadavg)
+  line = line + bold_start + "Uptime:" + bold_end + " " + str_uptime
+  jsonDict['uptime']  = str(str_uptime)
+  if not isJson:
+    print(line)
 
-    uptime = datetime.now() - datetime.fromtimestamp(psutil.boot_time())
-    str_uptime = str(uptime).split('.')[0]
-    line = ""
-    uname_len = 8
-    av1, av2, av3 = os.getloadavg()
-    str_loadavg = "%.2f %.2f %.2f  " % (av1, av2, av3)
-    line = bold_start + "Load average: " + bold_end + str_loadavg
-    jsonDict['load_avg'] = str(str_loadavg)
-    line = line + bold_start + "Uptime:" + bold_end + " " + str_uptime
-    jsonDict['uptime'] = str(str_uptime)
-    if not isJson:
-        print(line)
+  i = 0
+  my_pid = os.getpid()
+  if not isJson:
+    print("")
+    print(bold_start + "    PID " + "USER".ljust(uname_len) + "   %CPU %MEM      TIME+ COMMAND" + bold_end)
 
-    i = 0
-    my_pid = os.getpid()
-    if not isJson:
-        print("")
-        print(bold_start + "    PID " + "USER".ljust(uname_len) + "   %CPU %MEM      TIME+ COMMAND" + bold_end)
-
-    jsonList = []
-    for pp in processes:
-        if pp['pid'] == my_pid:
-            continue
+  jsonList = []
+  for pp in processes:
+    if pp['pid'] == my_pid:
+      continue
     i += 1
     if i > 10:
-        break
+      break
 
     # TIME+ column shows process CPU cumulative time and it
     # is expressed as: "mm:ss.ms"
@@ -192,30 +188,29 @@ def top(display=True, isJson=False):
         pp['username'] = username
         pp['ctime'] = ctime
         pp['cpu_percent'] = float(pp['cpu_percent'])
-        pp['memory_percent'] = float(round(pp['memory_percent'], 1))
+        pp['memory_percent'] = float(round(pp['memory_percent'],1))
         jsonList.append(pp)
     else:
-        print(
-                str(pp['pid']).rjust(7) + " " +
-                username.ljust(uname_len) + " " +
-                str(pp['cpu_percent']).rjust(6) + " " +
-                str(round(pp['memory_percent'], 1)).rjust(4) + " " +
-                str(ctime).rjust(10) + " " +
-                pp['name']
-        )
-    if isJson:
-        jsonDict['top'] = jsonList
-        print(json.dumps([jsonDict]))
-    else:
-        print("")
+ 
+      print( str(pp['pid']).rjust(7) + " " + \
+            username.ljust(uname_len) + " " + \
+            str(pp['cpu_percent']).rjust(6) + " " + \
+            str(round(pp['memory_percent'],1)).rjust(4) + " " + \
+            str(ctime).rjust(10) + " " + \
+            pp['name'] )
+  if isJson:
+      jsonDict['top'] = jsonList
+      print ( json.dumps([jsonDict]) )
+  else:
+    print( "" )
 
 
 def list(p_json, p_cat, p_comp, p_ver, p_port, p_status, p_kount):
-    lst = " "
-    if p_kount > 1:
-        lst = ","
-    if p_json:
-        lst = lst + \
+  lst = " "
+  if p_kount > 1:
+    lst = ","
+  if p_json:
+    lst = lst + \
       '{"category": "' + p_cat.rstrip() + '",' + \
       ' "component": "' + p_comp.rstrip() + '",' + \
       ' "version": "' + p_ver.rstrip() + '",' + \
@@ -224,20 +219,20 @@ def list(p_json, p_cat, p_comp, p_ver, p_port, p_status, p_kount):
     print(lst)
     return
 
-print(p_comp + "  " + p_ver + "  " + p_port + "  " + p_status)
+  print (p_comp + "  " +  p_ver + "  " + p_port + "  " + p_status)
 
 
 def status (p_json, p_comp, p_ver, p_state, p_port, p_kount):
-    status = " "
-    if p_kount > 1:
-        status = ","
-    if p_json:
-        jsonStatus = {}
-        jsonStatus['component'] = p_comp
-        jsonStatus['version'] = p_ver
-        jsonStatus['state'] = p_state
+  status = " "
+  if p_kount > 1:
+    status = ","
+  if p_json:
+    jsonStatus = {}
+    jsonStatus['component'] = p_comp
+    jsonStatus['version'] = p_ver
+    jsonStatus['state'] = p_state
     if p_port!="" and int(p_port)>1:
-        jsonStatus['port'] = p_port
+      jsonStatus['port'] = p_port
     category = util.get_comp_category(p_comp)
     if category:
       jsonStatus['category'] = category
@@ -249,7 +244,7 @@ def status (p_json, p_comp, p_ver, p_state, p_port, p_kount):
   app_ver = p_comp + "-" + p_ver
   app_ver = app_ver + (' ' * (35 - len(app_ver)))
 
-  if p_state in ("Running", "Stopped") and int(p_port) > 1:
+  if p_state in ("Running", "Stopped") and int(p_port)>1:
     on_port = " on port " + p_port
   else:
     on_port = ""
@@ -260,21 +255,22 @@ def status (p_json, p_comp, p_ver, p_state, p_port, p_kount):
 
 
 def info(p_json, p_home, p_repo, print_flag=True):
-    cloud_name, cloud_platform, instance_id, flavor, region, az, private_ip = util.get_cloud_info()
-    
-    p_user = util.get_user()
-    p_is_admin = util.is_admin()
-    pip_ver = get_pip_ver()
-    os_arch = util.get_arch()
 
-    this_os = ""
-    this_uname = str(platform.system())[0:7]
-    if private_ip > "":
-        host_ip = private_ip
-    else:
-        host_ip = util.get_host_ip()
-    wmic_path = os.getenv("SYSTEMROOT", "") + os.sep + "System32" + os.sep + "wbem" + os.sep + "wmic"
-    host_display = util.get_host_short()
+  cloud_name, cloud_platform, instance_id, flavor, region, az, private_ip = util.get_cloud_info()
+
+  p_user = util.get_user()
+  p_is_admin = util.is_admin()
+  pip_ver = get_pip_ver()
+  os_arch = util.get_arch()
+
+  this_os = ""
+  this_uname = str(platform.system())[0:7]
+  if private_ip > "":
+    host_ip = private_ip
+  else:
+    host_ip = util.get_host_ip()
+  wmic_path = os.getenv("SYSTEMROOT", "") + os.sep + "System32" + os.sep + "wbem" + os.sep + "wmic"
+  host_display = util.get_host_short()
 
   ## Check the OS & Resources ########################################
   plat = util.get_os()
@@ -412,14 +408,16 @@ def info(p_json, p_home, p_repo, print_flag=True):
   print(bold_start + "#          OS: " + bold_end + os2.rstrip() + " " + glibc_v_display + os_arch)
   print(bold_start + "#     Machine: " + bold_end + mem + ", " + cores + " vCPU, " + cpu)
   if instance_id > "" and not cloud_name == "unknown":
-      print(bold_start + "#  Cloud Info: " + bold_end +\
+    print(bold_start + "#  Cloud Info: " + bold_end +\
       f"{cloud_name}  {cloud_platform}  {instance_id}  {flavor}  {az}")
 
   print(bold_start + "#       Langs: " + bold_end + langs)
+
   print(bold_start + "#    Repo URL: " + bold_end + p_repo)
 
   if not last_update_local:
-      last_update_local = "None"
+    last_update_local="None"
+
   print(bold_start + "# Last Update: " + bold_end + str(last_update_local))
   print(bold_start + ("#" * 70) + bold_end)
 
@@ -428,12 +426,9 @@ def info_component(p_comp_dict, p_kount):
     if p_kount > 1:
         print(bold_start + ("-" * 90) + bold_end)
 
-    print(bold_start + "     Project: " + bold_end + p_comp_dict['project'] + " (" + p_comp_dict['project_url'] + ")")
+    print(bold_start + "     Project: " + bold_end + p_comp_dict['project'] + " (" + p_comp_dict['project_url'] + ")" )
 
-    print(
-            bold_start + "   Component: " + bold_end + p_comp_dict['component'] +
-            " " + p_comp_dict['version'] + " (" + p_comp_dict['proj_description'] + ")"
-    )
+    print(bold_start + "   Component: " + bold_end + p_comp_dict['component'] + " " + p_comp_dict['version'] + " (" + p_comp_dict['proj_description'] + ")")
 
     if p_comp_dict['port'] > 1:
         print(bold_start + "        port: " + bold_end + str(p_comp_dict['port']))
@@ -441,7 +436,7 @@ def info_component(p_comp_dict, p_kount):
     if p_comp_dict['datadir'] > "":
         print(bold_start + "     datadir: " + bold_end + p_comp_dict['datadir'])
 
-    if p_comp_dict['logdir'] > "":
+    if p_comp_dict['logdir']  > "":
         print(bold_start + "      logdir: " + bold_end + p_comp_dict['logdir'])
 
     if p_comp_dict['autostart'] == "on":
@@ -450,11 +445,9 @@ def info_component(p_comp_dict, p_kount):
     if p_comp_dict['svcuser'] > "" and util.get_platform() == "Linux":
         print(bold_start + "     svcuser: " + bold_end + p_comp_dict['svcuser'])
 
-    if (('status' in p_comp_dict) and ('up_time' in p_comp_dict)):
-        print(
-                bold_start + "      status: " + bold_end + p_comp_dict['status'] +
-                bold_start + " for " + bold_end + p_comp_dict['up_time']
-        )
+    if (('status' in p_comp_dict)  and ('up_time' in p_comp_dict)):
+        print(bold_start + "      status: " + bold_end + p_comp_dict['status'] + \
+              bold_start + " for " + bold_end + p_comp_dict['up_time'])
     else:
         if 'status' in p_comp_dict:
             print(bold_start + "      status: " + bold_end + p_comp_dict['status'])
@@ -467,38 +460,32 @@ def info_component(p_comp_dict, p_kount):
     if 'connections' in p_comp_dict:
         print(bold_start + " connections: " + bold_end + p_comp_dict['connections'])
 
-    print(
-            bold_start + "Release Date: " + bold_end + p_comp_dict['release_date'] +
-            bold_start + "  Stage: " + bold_end + p_comp_dict['stage']
-    )
+    print(bold_start + "Release Date: " + bold_end + p_comp_dict['release_date'] + \
+          bold_start + "  Stage: " + bold_end + p_comp_dict['stage'])
 
     if p_comp_dict['platform'] > "":
-        print(bold_start + "Supported On: " + bold_end + "[" + p_comp_dict['platform'] + "]")
+      print(bold_start + "Supported On: " + bold_end + "[" + p_comp_dict['platform'] + "]")
 
     if p_comp_dict['pre_reqs'] > "":
-        print(bold_start + "   Pre Req's: " + bold_end + p_comp_dict['pre_reqs'])
+      print(bold_start + "   Pre Req's: " + bold_end + p_comp_dict['pre_reqs'])
 
-    print(bold_start + "     License: " + bold_end + p_comp_dict['license'])
+    print(bold_start +   "     License: " + bold_end + p_comp_dict['license'])
 
     is_installed = str(p_comp_dict['is_installed'])
     if str(is_installed) == "0":
-        is_installed = "NO"
+       is_installed = "NO"
 
-    print(
-          bold_start + "   IsCurrent: " + bold_end + str(p_comp_dict['is_current']) +
-          bold_start + "  IsInstalled: " + bold_end + is_installed
-    )
+    print(bold_start +   "   IsCurrent: " + bold_end + str(p_comp_dict['is_current']) + \
+          bold_start +   "  IsInstalled: " + bold_end + is_installed)
 
 
-def format_data_to_table(
-                    data,
+def format_data_to_table(data,
                     keys,
                     header=None,
                     error_key=None,
                     error_msg_column=None,
                     sort_by_key=None,
-                    sort_order_reverse=False
-):
+                    sort_order_reverse=False):
     """Takes a list of dictionaries, formats the data, and returns
     the formatted data as a text table.
 
@@ -530,8 +517,8 @@ def format_data_to_table(
         # Create a list of dictionary from the keys and the header and
         # insert it at the beginning of the list. Do the same for the
         # divider and insert below the header.
-        # header_divider = dict(zip(keys, header_divider))
-        # data.insert(0, header_divider)
+        #header_divider = dict(zip(keys, header_divider))
+        #data.insert(0, header_divider)
         header = dict(zip(keys, header))
         data.insert(0, header)
 
@@ -541,29 +528,30 @@ def format_data_to_table(
 
     # Create a tuple pair of key and the associated column width for it
     key_width_pair = zip(keys, column_widths)
-    key_length = len(keys)
+    key_length =  len(keys)
+
     str_format = ('%-*s ' * len(keys)).strip() + '\n'
     formatted_data = ''
 
     for element in data:
         data_to_format = []
-        s = 0
+        s=0
         key_width_pair = zip(keys, column_widths)
         # Create a tuple that will be used for the formatting in
         # width, value format
         for pair in key_width_pair:
             dataStr = str(element[pair[0]])
             spaces = " " * ((int(float(pair[1])) - len(dataStr))-2)
-            if s < key_length - 1:
+            if s<key_length-1:
                 spaces = spaces + " |"
 
             if dataStr in header.values():
-                if s == 0:
+                if s==0:
                     dataStr = table_header_style + dataStr
-                dataStr = dataStr + spaces
-                if s == key_length - 1:
-                    dataStr = dataStr + bold_end
-                s = s + 1
+                dataStr += spaces
+                if s==key_length-1:
+                    dataStr += bold_end
+                s=s+1
             elif (error_key and error_msg_column):
                 if pair[0] in error_msg_column and element.get(error_key[0]) == error_key[1]:
                     dataStr = error_start + dataStr + bold_end
