5e238bb8b94dc6c8034aa2ecd9c5106808862533
Denis Lussier <denis@lussier.io>
2023-12-04 21:00:11 -0500

    open up 24.1.3 for dev

--- cli/scripts/install24.py
+++ cli/scripts/install24.py
@@ -4,7 +4,7 @@
 
 import sys, os
 
-VER = "24.1.2"
+VER = "24.1.3"
 REPO = os.getenv("REPO", "https://pgedge-upstream.s3.amazonaws.com/REPO")
 
 if sys.version_info < (3, 6):
--- src/conf/versions24.sql
+++ src/conf/versions24.sql
@@ -85,7 +85,8 @@ INSERT INTO categories VALUES (9,  87, 'Management & Monitoring', 'Manage/Monito
 -- ## HUB ################################
 INSERT INTO projects VALUES ('hub', 'app', 0, 0, 'hub', 0, 'https://github.com/pgedge/nodectl','',0,'','','');
 INSERT INTO releases VALUES ('hub', 1, 'hub', '', '', 'hidden', '', 1, '', '', '');
-INSERT INTO versions VALUES ('hub', '24.1.2',  '',  1, '20231204', '', '', '');
+INSERT INTO versions VALUES ('hub', '24.1.3',  '',  1, '20231215', '', '', '');
+INSERT INTO versions VALUES ('hub', '24.1.2',  '',  0, '20231204', '', '', '');
 INSERT INTO versions VALUES ('hub', '24.1.1',  '',  0, '20231130', '', '', '');
 INSERT INTO versions VALUES ('hub', '23.134',  '',  0, '20231013', '', '', '');
 
