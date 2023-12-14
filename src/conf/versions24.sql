f2d96fa30f8d79870a68b047b88090f33d6a1f84
Denis Lussier <denis@lussier.io>
2023-12-14 20:57:46 +0000

    fix missing pg16 on el8 entry and other cleanups

--- src/conf/versions24.sql
+++ src/conf/versions24.sql
@@ -98,14 +98,10 @@ INSERT INTO projects VALUES ('pg', 'pge', 1, 5432, 'hub', 1, 'https://github.com
 INSERT INTO releases VALUES ('pg12', 3, 'pg', 'PostgreSQL', '', 'prod',
   '<font size=-1>New in <a href=https://www.postgresql.org/docs/12/release-12.html>2019</a></font>', 1, 'POSTGRES', '', '');
 INSERT INTO versions VALUES ('pg12', '12.17-1', 'el8', 1, '20231109', '', '', '');
-INSERT INTO versions VALUES ('pg12', '12.16-1', 'el8', 0, '20230810', '', '', '');
-INSERT INTO versions VALUES ('pg12', '12.15-1', 'el8', 0, '20230511', '', '', '');
 
 INSERT INTO releases VALUES ('pg13', 2, 'pg', '', '', 'prod',
   '<font size=-1>New in <a href=https://www.postgresql.org/docs/13/release-13.html>2020</a></font>', 1, 'POSTGRES', '', '');
 INSERT INTO versions VALUES ('pg13', '13.13-1', 'el8', 1, '20231109','', '', '');
-INSERT INTO versions VALUES ('pg13', '13.12-1', 'el8', 0, '20230810','', '', '');
-INSERT INTO versions VALUES ('pg13', '13.11-1', 'el8', 0, '20230511','', '', '');
 
 INSERT INTO releases VALUES ('pg14', 1, 'pg', '', '', 'prod', 
   '<font size=-1>New in <a href=https://www.postgresql.org/docs/14/release-14.html>2021</a></font>', 1, 'POSTGRES', '', '');
@@ -114,13 +110,10 @@ INSERT INTO versions VALUES ('pg14', '14.10-1', 'el8, el9, arm9', 1, '20231129',
 INSERT INTO releases VALUES ('pg15', 2, 'pg', '', '', 'prod', 
   '<font size=-1>New in <a href=https://www.postgresql.org/docs/15/release-15.html>2022</a></font>', 1, 'POSTGRES', '', '');
 INSERT INTO versions VALUES ('pg15', '15.5-1',  'el8, el9, arm9', 1, '20231109','', '', '');
-INSERT INTO versions VALUES ('pg15', '15.4-2',  'el8, el9, arm9', 0, '20230829','', '', '');
 
 INSERT INTO releases VALUES ('pg16', 2, 'pg', '', '', 'prod', 
   '<font size=-1>New in <a href=https://www.postgresql.org/docs/16/release-16.html>2023!</a></font>', 1, 'POSTGRES', '', '');
-INSERT INTO versions VALUES ('pg16', '16.1-1',    'el9, arm9', 1, '20231109','', '', '');
-INSERT INTO versions VALUES ('pg16', '16.0-2',    'el9, arm9', 0, '20231021','', '', '');
-INSERT INTO versions VALUES ('pg16', '16.0-1',    'el9, arm9', 0, '20230914','', '', '');
+INSERT INTO versions VALUES ('pg16', '16.1-1',  'el8, el9, arm9', 1, '20231109','', '', '');
 
 INSERT INTO projects VALUES ('orafce', 'ext', 4, 0, 'hub', 0, 'https://github.com/orafce/orafce/releases',
   'orafce', 1, 'larry.png', 'Ora Built-in Packages', 'https://github.com/orafce/orafce#orafce---oracles-compatibility-functions-and-packages');
@@ -265,9 +258,6 @@ INSERT INTO releases VALUES ('spock32-pg16', 4, 'spock', 'Spock', '', 'prod', ''
 INSERT INTO versions VALUES ('spock32-pg14', '3.2.2-1', 'el8, el9, arm9', 1, '20231204', 'pg14', '', '');
 INSERT INTO versions VALUES ('spock32-pg15', '3.2.2-1', 'el8, el9, arm9', 1, '20231204', 'pg15', '', '');
 INSERT INTO versions VALUES ('spock32-pg16', '3.2.2-1', 'el8, el9, arm9', 1, '20231204', 'pg16', '', '');
-INSERT INTO versions VALUES ('spock32-pg14', '3.2.1-1', 'el9, arm9', 0, '20231130', 'pg14', '', '');
-INSERT INTO versions VALUES ('spock32-pg15', '3.2.1-1', 'el9, arm9', 0, '20231130', 'pg15', '', '');
-INSERT INTO versions VALUES ('spock32-pg16', '3.2.1-1', 'el9, arm9', 0, '20231130', 'pg16', '', '');
 
 INSERT INTO projects VALUES ('pglogical', 'ext', 4, 0, 'hub', 1, 'https://github.com/2ndQuadrant/pglogical/releases',
   'pglogical', 1, 'spock.png', 'Logical Replication', 'https://github.com/2ndQuadrant/pglogical');
