40cbf1cdb7fab58b632d22fd453206d7e3e94f55
Denis Lussier <denis@lussier.io>
2024-01-26 14:13:22 +0000

    more cleanup for setup

--- devel/setup/1-cli.sh
+++ devel/setup/1-cli.sh
@@ -1,7 +1,12 @@
 
 set -x
 
-./1a-python39.sh
+grep /etc/os-release el8
+rc=$?
+if [ "$rc" == "0" ]; then
+  ./1a-python39.sh
+fi
+
 ./1b-pip3.sh 
 ./1c-bashrc.sh 
 ./1d-awscli.sh
--- devel/setup/1c-bashrc.sh
+++ devel/setup/1c-bashrc.sh
@@ -5,10 +5,10 @@ else
   bf=~/.bash_profile
 fi
 
-grep CT $bf > /dev/null 2>&1
+grep PGE $bf > /dev/null 2>&1
 rc=$?
 if [ "$rc" == "0" ]; then
-  echo "Your $bf appears to already be configured for NodeCtl"
+  echo "Your $bf appears to already be configured for CLI"
 else
   cat bash_profile >> $bf
   echo "# run \"source $bf\" to load revised profile without re-booting"
