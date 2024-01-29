ca8fb3d7ee4781087257ab072d4cda29f8c314a8
Denis Lussier <denis@lussier.io>
2024-01-29 16:50:12 +0000

    wip

--- devel/setup/x.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.name "Denis Lussier"
-git config --global user.email denis@lussier.io
-
-git config --global push.default simple
-git config --global credential.helper store
-git config --global pull.rebase false
