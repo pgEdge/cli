2607a845d8023bde476767f125fcd999628c7440
denis lussier <denis@lussier.io>
2024-02-25 09:30:06 -0500

    wip vscode

--- cli/scripts/debug.py
+++ cli/scripts/debug.py
@@ -8,9 +8,9 @@ os.environ["MY_LITE"] = f"{os.getenv('PGE')}/out/posix/conf/db_local.db"
 
 import util, multicloud
 
-multicloud.list_nodes("akm")
+#multicloud.list_nodes("akm")
 #multicloud.list_zones("akm")
 #multicloud.list_airport_regions(airport="iad")
 #multicloud.list_sizes(provider="akm")
 #multicloud.create_node(provider="akm", region="us-east", name="ewr-akm-n222", ssh_key="eqn-test-key")
-#multicloud.list_keys("akm")
+multicloud.list_keys("eqn", project="2b293326-e54c-45a9-a248-c2e018a4429e")
