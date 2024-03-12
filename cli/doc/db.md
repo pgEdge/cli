04b35aff940d9e051c6c803c2e6289f97d9ff3d0
Cady Motyka <cadymo@Cadys-MacBook-Pro.local>
2024-03-12 11:14:07 -0400

    cleanup db and docs

--- cli/doc/db.md
+++ cli/doc/db.md
@@ -6,6 +6,3 @@
      create              # Create a pg db with spock installed into it.
      guc-set             # Set GUC
      guc-show            # Show GUC
-     dump                # Dump a database, schema, object from the source dsn to a file
-     restore             # Restore a database, schema, object from a file to the target_dsn
-     migrate             # Migrate a database, schema, object from a source_dsn to the target_dsn
--- cli/scripts/db.py
+++ cli/scripts/db.py
@@ -139,107 +139,11 @@ def guc_show(guc_name, pg=None):
     sys.exit(0)
 
 
-def dump(object, source_dsn, file="/tmp/db_0.sql", schema_only=False, pg=None):
-    """Dump a database, schema, object from the source dsn to a file
-
-    object: database or database.schema.object where schema and object can contain wildcard '*'
-    source_dsn: host=x, port=x, username=x (in any order)
-    file: location and file name for dump
-    schema_only: do not include data in the pg_dump
-    """
-    pg_v = util.get_pg_v(pg)
-
-    cmd = f'./pgedge pgbin {pg_v} "pg_dump '
-
-    if "=" in source_dsn:
-        if "," in source_dsn:
-            host = source_dsn.split("host=")[1].split(",")[0]
-            port = source_dsn.split("port=")[1].split(",")[0]
-            user = source_dsn.split("user=")[1].split(",")[0]
-            cmd = f"{cmd} -h {host} -p {port} -U {user} "
-        else:
-            util.exit_message(
-                "source_dsn must be in format: host=x, port=x, username=x, password=x"
-            )
-    else:
-        util.exit_message(
-            "source_dsn must be in format: host=x, port=x, username=x, password=x"
-        )
-
-    if "." in object:
-        db = object.split(".")[0]
-        schema = object.split(".")[1]
-        table = object.split(".")[2]
-        cmd = f"{cmd} --schema='{schema}' --table='{table}' {db}"
-    else:
-        cmd = f"{cmd} {object} -N spock -N pg_catalog "
-
-    if schema_only is True:
-        cmd = f"{cmd} --schema-only"
-
-    cmd = f'{cmd} --clean -f {file} "'
-    print(cmd)
-    os.system(cmd)
-
-
-def restore(object, target_dsn, file="/tmp/db_0.sql", pg=None):
-    """Restore a database, schema, object from a file to the target_dsn
-
-    object: database.schema.object where schema and object can contain wildcard '*'
-    target_dsn: host=x, port=x, username=x, database=x (in any order)
-    file: location and file name for dump
-    """
-
-    os.system(f"cat {file} | grep -v -E '(spock)' > /tmp/db_1.sql")
-    file = "/tmp/db_1.sql"
-
-    pg_v = util.get_pg_v(pg)
-    cmd = f'./pgedge pgbin {pg_v} "psql '
-
-    if "=" in target_dsn:
-        if "," in target_dsn:
-            host = target_dsn.split("host=")[1].split(",")[0]
-            port = target_dsn.split("port=")[1].split(",")[0]
-            user = target_dsn.split("user=")[1].split(",")[0]
-            cmd = f"{cmd} -h {host} -p {port} -U {user} "
-        else:
-            util.exit_message(
-                "target_dsn must be in format: host=x, port=x, username=x, password=x"
-            )
-    else:
-        util.exit_message(
-            "target_dsn must be in format: host=x, port=x, username=x, password=x"
-        )
-
-    if "." in object:
-        db = object.split(".")[0]
-    else:
-        db = object
-
-    cmd = f'{cmd} {db} -f {file} "'
-    print(cmd)
-    os.system(cmd)
-
-
-def migrate(object, source_dsn, target_dsn, schema_only=False, pg=None):
-    """Migrate a database, schema, object from a source_dsn to the target_dsn
-
-    object: database.schema.object where schema and object can contain wildcard '*'
-    source_dsn: host=x, port=x, username=x, password=x, database=x (in any order)
-    target_dsn: host=x, port=x, username=x, password=x, database=x (in any order)
-    schema_only: do not include data in the pg_dump
-    """
-    pass
-
-
 if __name__ == "__main__":
     fire.Fire(
         {
             "create": create,
             "guc-set": guc_set,
             "guc-show": guc_show,
-            "dump": dump,
-            "restore": restore,
-            "migrate": migrate,
         }
     )
