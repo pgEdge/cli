bdb070339b0b5d89d49e4451a96f6212c1aa1012
hayee-bhatti <152845623+hayee-bhatti@users.noreply.github.com>
2024-03-22 19:08:45 +0500

    Introduced a new schedule for auto-DDL tests. (#101)
    Includes setup tasks:
     -Creating nodes and subscriptions, Enabling autoddl GUCs.
    Includes teardown tasks:
     -Dropping subscriptions and nodes, Disabling GUCs.

---
+++ test/t/6011_setup_autoddl_gucs_on_n2.pl
@@ -0,0 +1,52 @@
+# This testcase enables the the two primary auto ddl gucs 
+# spock.enable_ddl_replication AND spock.include_ddl_repset
+# on node 2 and performs a reload.
+#
+use strict;
+use warnings;
+use File::Which;
+use IPC::Cmd qw(run);
+use Try::Tiny;
+use JSON;
+use lib './t/lib';
+use contains;
+use edge;
+
+# Our parameters are:
+
+my $homedir2="$ENV{EDGE_CLUSTER_DIR}/n2/pgedge";
+my $host = $ENV{EDGE_HOST};
+my $port = $ENV{'EDGE_START_PORT'} + 1;
+my $database = $ENV{EDGE_DB};
+print("The home directory of node 2 is $homedir2\n");
+
+# command to enable spock.enable_ddl_replication 
+my $enableDDLRep = qq(alter system set spock.enable_ddl_replication = on);
+# command to enable spock.include_ddl_repset
+my $includeDDLRep = qq(alter system set spock.include_ddl_repset = on);
+# execute the two alter syste commands followed by a server reload
+# all commands cannot be combined in a single -c switch
+my $cmd1 = qq($homedir2/$ENV{EDGE_COMPONENT}/bin/psql -t -h $host -p $port -d $database -c "$enableDDLRep" -c "$includeDDLRep" -c "select pg_reload_conf()");
+print("cmd1 = $cmd1\n");
+my $stdout_buf= (run_command_and_exit_iferr ($cmd1))[3];
+print("stdout_buf = @$stdout_buf\n");
+sleep(0.5); #for the reload to be ready
+
+# Validate the two gucs are ON
+my $cmd2 = qq($homedir2/$ENV{EDGE_COMPONENT}/bin/psql -t -h $host -p $port -d $database -c "show spock.enable_ddl_replication" -c "show spock.include_ddl_repset");
+print("cmd2 = $cmd2\n");
+my $stdout_buf1= (run_command_and_exit_iferr ($cmd2))[3];
+#combining a multi line output into a single line to ensure consistent comparisons to avoid random \n messing up comparisons
+@$stdout_buf1 = join(' ', map { sanitize_and_combine_multiline_stdout($_) } @$stdout_buf1);
+print("stdout_buf1 = @$stdout_buf1\n");
+
+if (contains($stdout_buf1->[0], "on on"))
+{
+    print("AutoDDL Gucs spock.enable_ddl_replication and spock.include_ddl_repset are now enabled");
+    exit(0);
+}
+else
+{
+    print("AutoDDL Gucs spock.enable_ddl_replication and spock.include_ddl_repset are NOT enabled. Exiting with failure");
+    exit(1);
+}
