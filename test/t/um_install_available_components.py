9e00bbdb9887dfa63e19793e113007c045336d77
susan <susan@localhost.localdomain>
2024-05-02 15:27:04 -0400

    Cleaning up environment variables in UM test

--- test/t/um_install_available_components.py
+++ test/t/um_install_available_components.py
@@ -6,18 +6,11 @@ print(f"Starting - {os.path.basename(__file__)}")
 # Get Test Settings
 util_test.set_env()
 repo=os.getenv("EDGE_REPO")
-num_nodes=int(os.getenv("EDGE_NODES",2))
 cluster_dir=os.getenv("EDGE_CLUSTER_DIR")
 port=int(os.getenv("EDGE_START_PORT",6432))
 usr=os.getenv("EDGE_USERNAME","lcusr")
 pw=os.getenv("EDGE_PASSWORD","password")
-db=os.getenv("EDGE_DB","demo")
-host=os.getenv("EDGE_HOST","localhost")
-repuser=os.getenv("EDGE_REPUSER","repuser")
-repset=os.getenv("EDGE_REPSET","demo-repset")
-spockpath=os.getenv("EDGE_SPOCK_PATH")
 dbname=os.getenv("EDGE_DB","lcdb")
-home_dir=os.getenv("EDGE_HOME_DIR", "nc/pgedge")
 
 # Get a list of available packages - the list is returned in a json string:
 get_list=("um list --json")
