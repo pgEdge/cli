7269ca888c108729c98f91404ddc6de3c87b33b2
susan <susan@localhost.localdomain>
2024-05-02 15:23:12 -0400

    Checking in UM work - still WIP

---
+++ test/t/um_install_available_components.py
@@ -0,0 +1,48 @@
+import sys, os, util_test, subprocess, json
+
+# Print Script
+print(f"Starting - {os.path.basename(__file__)}")
+
+# Get Test Settings
+util_test.set_env()
+repo=os.getenv("EDGE_REPO")
+num_nodes=int(os.getenv("EDGE_NODES",2))
+cluster_dir=os.getenv("EDGE_CLUSTER_DIR")
+port=int(os.getenv("EDGE_START_PORT",6432))
+usr=os.getenv("EDGE_USERNAME","lcusr")
+pw=os.getenv("EDGE_PASSWORD","password")
+db=os.getenv("EDGE_DB","demo")
+host=os.getenv("EDGE_HOST","localhost")
+repuser=os.getenv("EDGE_REPUSER","repuser")
+repset=os.getenv("EDGE_REPSET","demo-repset")
+spockpath=os.getenv("EDGE_SPOCK_PATH")
+dbname=os.getenv("EDGE_DB","lcdb")
+home_dir=os.getenv("EDGE_HOME_DIR", "nc/pgedge")
+
+# Get a list of available packages - the list is returned in a json string:
+get_list=("um list --json")
+res=util_test.run_cmd("Getting list of available packages", get_list, f"{cluster_dir}/n1")
+# Break the json string into a list:
+res = json.loads(res.stdout)
+
+# Loop through the results and install each available component:
+for i, inner_list in enumerate(res): 
+    comp=(res[i].get("component"))
+    install_comp=(f"um install {comp} -U {usr} -P {pw} -d {dbname}")
+    inst_res=util_test.run_cmd("Getting list of available packages", install_comp, f"{cluster_dir}/n1")
+
+# List the installed components:
+installed=(f"um list")
+installed_res=util_test.run_cmd("Getting list of available packages", installed, f"{cluster_dir}/n1")
+print(installed_res.stdout)
+
+
+# Needle and Haystack
+#Check for a returncode to confirm results:
+
+if installed_res.returncode != 0:
+    util_test.EXIT_FAIL
+else:
+    util_test.EXIT_PASS
+
+
