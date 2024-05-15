5b17372f53be2771e4c1fda38ebb8e0a907700c5
A. Hayee Bhatti <hayee@pgedge.com>
2024-05-15 16:43:51 +0500

    Enabling allow_ddl_from_functions in the autoddl setup tests

--- test/t/6011_setup_autoddl_gucs_on_n2.pl
+++ test/t/6011_setup_autoddl_gucs_on_n2.pl
@@ -24,29 +24,31 @@ print("The home directory of node 2 is $homedir2\n");
 my $enableDDLRep = qq(alter system set spock.enable_ddl_replication = on);
 # command to enable spock.include_ddl_repset
 my $includeDDLRep = qq(alter system set spock.include_ddl_repset = on);
+# command to enable spock.allow_ddl_from_functions
+my $allowDDLFunc = qq(alter system set spock.allow_ddl_from_functions = on);
 # execute the two alter syste commands followed by a server reload
 # all commands cannot be combined in a single -c switch
-my $cmd1 = qq($homedir2/$ENV{EDGE_COMPONENT}/bin/psql -t -h $host -p $port -d $database -c "$enableDDLRep" -c "$includeDDLRep" -c "select pg_reload_conf()");
+my $cmd1 = qq($homedir2/$ENV{EDGE_COMPONENT}/bin/psql -t -h $host -p $port -d $database -c "$enableDDLRep" -c "$includeDDLRep" -c "$allowDDLFunc" -c "select pg_reload_conf()");
 print("cmd1 = $cmd1\n");
 my $stdout_buf= (run_command_and_exit_iferr ($cmd1))[3];
 print("stdout_buf = @$stdout_buf\n");
-sleep(0.5); #for the reload to be ready
+sleep(0.75); #for the reload to be ready
 
 # Validate the two gucs are ON
-my $cmd2 = qq($homedir2/$ENV{EDGE_COMPONENT}/bin/psql -t -h $host -p $port -d $database -c "show spock.enable_ddl_replication" -c "show spock.include_ddl_repset");
+my $cmd2 = qq($homedir2/$ENV{EDGE_COMPONENT}/bin/psql -t -h $host -p $port -d $database -c "show spock.enable_ddl_replication" -c "show spock.include_ddl_repset" -c "show spock.allow_ddl_from_functions");
 print("cmd2 = $cmd2\n");
 my $stdout_buf1= (run_command_and_exit_iferr ($cmd2))[3];
 #combining a multi line output into a single line to ensure consistent comparisons to avoid random \n messing up comparisons
 @$stdout_buf1 = join(' ', map { sanitize_and_combine_multiline_stdout($_) } @$stdout_buf1);
 print("stdout_buf1 = @$stdout_buf1\n");
 
-if (contains($stdout_buf1->[0], "on on"))
+if (contains($stdout_buf1->[0], "on on on"))
 {
-    print("AutoDDL Gucs spock.enable_ddl_replication and spock.include_ddl_repset are now enabled");
+    print("AutoDDL Gucs spock.enable_ddl_replication, spock.include_ddl_repset and spock.allow_ddl_from_functions are now enabled");
     exit(0);
 }
 else
 {
-    print("AutoDDL Gucs spock.enable_ddl_replication and spock.include_ddl_repset are NOT enabled. Exiting with failure");
+    print("One or more of AutoDDL Gucs could not be enabled. Exiting with failure");
     exit(1);
 }
