0fc2465247d293c200f3d05234974a009188ea49
Denis Lussier <denis@lussier.io>
2024-05-22 14:27:52 +0000

    open up 24.5.3 for development work

--- cli/scripts/util.py
+++ cli/scripts/util.py
@@ -3,7 +3,7 @@
 import os
 import time
 
-MY_VERSION = "24.5.2"
+MY_VERSION = "24.5.3"
 DEFAULT_PG = "16"
 DEFAULT_SPOCK = "33"
 DEFAULT_SPOCK_17 = "40"
--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 
 bundle=pgedge
 api=pgedge
-hubV=24.5.2
+hubV=24.5.3
 ctlibsV=1.2
 
 spock40V=4.0.142-1
