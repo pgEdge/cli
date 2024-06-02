64f845227e654670a6d7e782451fbc13ecb5da18
Denis Lussier <denis@lussier.io>
2024-06-02 00:40:24 +0000

    support apt setups for python39+

--- devel/setup/1a-python39+.sh
+++ devel/setup/1a-python39+.sh
@@ -9,11 +9,28 @@ echo "# py3M = $py3M"
 
 set -x
 
-if [ "$py3M" == "3.6" ]; then
-   sudo yum install -y python39 python39-devel
+um=yum
+apt --version
+rc=$?
+if [ $rc == "0" ]; then
+  um=apt
 fi
 
-sudo yum install python3-devel gcc
+if [ "$py3M" == "3.6" ]; then
+   if [ "$um" == "yum" ]; then
+     sudo yum install -y python39 python39-devel python39-pip gcc
+   else
+     echo "Platform not supported"
+     exit 1
+   fi
+else
+   if [ "$um" == "yum" ]; then
+     sudo yum install -y python3-devel python3-pip gcc
+   else
+     sudo apt install -y python3-dev python3-pip python3-venv gcc
+   fi
+fi
+
 
 cd ~
 python3 -m venv venv
