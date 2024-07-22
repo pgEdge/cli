c84dee6074d5aab94517a5de862cda6afef41b17
Denis Lussier <denis@lussier.io>
2024-07-22 21:01:52 +0000

    buzzword WIP - pgml nvidia cuda ai

--- devel/setup/1e-nvidia.sh
+++ devel/setup/1e-nvidia.sh
@@ -3,7 +3,7 @@ set -x
 tk_v=12.5.1
 url=https://developer.download.nvidia.com/compute/cuda
 
-rpm_f=cuda-repo-rhel8-12-5-local-${tk_v}_555.42.06-1.x86_64.rpm
+rpm_f=cuda-repo-rhel8-12-5-local-${tk_v}_555.42.06-1.`arch`.rpm
 
 rm -f $rpm_f
 wget $url/$tk_v/local_installers/$rpm_f
