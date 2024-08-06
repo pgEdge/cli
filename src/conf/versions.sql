822485a249e574c92e7e348d45b99c1186e87155
Denis Lussier <denis@lussier.io>
2024-08-06 13:53:06 +0000

    oops, bump pg17 to beta3

--- src/conf/versions.sql
+++ src/conf/versions.sql
@@ -169,7 +169,7 @@ INSERT INTO versions VALUES ('pg16', '16.3-3',  'amd, arm', 0, '20240710','', ''
 
 INSERT INTO releases VALUES ('pg17', 2, 'pg', '', '', 'test', 
   '<font size=-1>New in <a href=https://www.postgresql.org/docs/17/release-17.html>2024!</a></font>', 1, 'POSTGRES', '', '');
-INSERT INTO versions VALUES ('pg17', '17beta2-3',  'amd, arm', 1, '20240808','', '', '');
+INSERT INTO versions VALUES ('pg17', '17beta3-1',  'amd, arm', 1, '20240808','', '', '');
 
 -- ## ORAFCE #############################
 INSERT INTO projects VALUES ('orafce', 'ext', 4, 0, 'hub', 0, 'https://github.com/orafce/orafce/releases',
