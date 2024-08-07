86c18aea8052a00834e20d13f01818ad7010ded9
Denis Lussier <denis@lussier.io>
2024-08-07 19:19:56 -0400

    wip

--- devel/docker/el9/run.sh
+++ devel/docker/el9/run.sh
@@ -1,8 +1,8 @@
 
 source env.sh
 
-podman stop -l
-podman run -d --rm --hostname $host --security-opt label:disable -p $port:22 $host
+podman stop $host
+podman run -d --rm --name $host --hostname $host --security-opt label:disable -p $port:22 $host
 sleep 1
 ssh-keygen -f ~/.ssh/known_hosts -R "[localhost]:$port"
 ssh-copy-id -o StrictHostKeyChecking=accept-new -p $port build@localhost >/dev/null 2>&1
