688cb234ea2457fd6b34e46aaa0107abad64528a
Denis Lussier <denis@lussier.io>
2024-08-09 17:53:00 -0400

    wip update-cli.py

--- cli/scripts/update-cli.py
+++ cli/scripts/update-cli.py
@@ -13,69 +13,72 @@ def get_now():
     return(datetime.datetime.now(datetime.UTC).astimezone().strftime("%Y%m%d_%H%M%S"))
 
 
-def get_cli_ver(home_dir=os.getenv("MY_HOME", "")):
-    util_py = f"{home_dir}/hub/scripts/util.py"
-    if not os.path.isfile(f"{util_py}"):
-        util.exit_message(f"Cannot locate version file: {util_py}")
+def get_cli_ver(p_file):
+
+    if not os.path.isfile(f"{p_file}"):
+        util.exit_message(f"Cannot locate version file: {p_file}")
 
     awk3 = "awk '{print $3}'"
-    ver_quoted = util.getoutput(f"grep 'MY_VERSION =' {util_py} | {awk3}")
+    ver_quoted = util.getoutput(f"grep 'MY_VERSION =' {p_file} | {awk3}")
 
     ver = ver_quoted.replace('"', '')
 
     return(util.format_ver(ver))
 
 
+def replace_files(from_dir, to_dir):
+    return
+
+
+def download_file(p_file, p_dir):
+    return
+
+
 def backup_current():
     ts = get_now()
-    ver = get_cli_ver()
+    ver = get_cli_ver({MY_HOME}/hub/scripts/install.py)
     backup_dir = f"{BASE_BACKUP_DIR}/{ts}_{ver}_BACKUP"
-    rc = util.echo_cmd(f"mkdir -p {backup_dir}")
+    rc = os.system(f"mkdir -p {backup_dir}")
     if rc != 0:
-        return(False)
+        return(None)
 
-    rc1 = util.echo_cmd(f"cp -r {MY_HOME}/hub {backup_dir}")
-    rc2 = util.echo_cmd(f"cp {MY_HOME}/pgedge {backup_dir}/.")
+    rc1 = os.system(f"cp -r {MY_HOME}/hub {backup_dir}")
+    rc2 = os.system(f"cp {MY_HOME}/pgedge {backup_dir}/.")
 
     if rc1 + rc2 == 0:
-        return(True)
+        return(backup_dir)
 
-    return(False)
-
-
-def download_new():
-    return("YYYY-MM-DD_HH-MM-SS_xx.yy-z_NEW")
+    return(None)
 
 
-def apply_new(new_dir=None):
-    if new_dir:
-       if not os.path_isdir(new_dir):
-           util.exit_message(f"Invalid new directory: {new_dir}")
+def apply_latest():
 
-    backup_dir = current_backup()
+    ver_current = get_cli_ver(f"{MY_HOME}/hub/scripts/install.py")
+    backup_dir = backup_current()
     if backup_dir is None:
         util.exit_message("Failed to take a current cli backup")
 
-    util.message("backup_dir = {backup_dir}")
+    download_dir = f"{BASE_BACKUP_DIR}/{get_now()}_{get_cli_ver()}_DOWNLOAD"
+    os.system(f"rm -rf {download_dir}")
+    os.system(f"mkdir {download_dir}")
+    download_file("install.py", download_dir)
+    ver_latest = get_cli_ver(f"{download_dir}/install.py")
 
-    if new_dir is None:
-        new_dir = new_download()
-        if new_dir is None:
-            util.exit_message("Failed to download a new dir")
+    file = f"hub-{verlatest}"
+    download_file(file, download_dir)
 
-    util.message(f"new_dir = {new_dir}")
+    #expand_file(file, download_dir)
 
-    ## copy new_dir over current_dir
-    copy_dir_over_current(new_dir)
+    #replace_files(download_dir, MY_HOME)
 
-    return(True)
+    return
 
 
-def list_dir_descending(base_dir, wildcard):
+def list_archives(base_dir, wildcard):
     dir_list = []
 
-    for name in os.listdir(base_dir):
-        if wildcard in name:
+    for name in os.listdir(BASE_BACKUP_DIR):
+        if ["_BACKUP", "_DOWNLOAD"] in name:
             dir_list.append(name)
 
     if len(dir_list) >= 1:
@@ -84,18 +87,7 @@ def list_dir_descending(base_dir, wildcard):
             print(name)
 
 
-def list_downloads():
-    list_dir_descending(BASE_BACKUP_DIR, "_DOWNLOAD")
-    return
-
-
-def list_backups():
-    list_dir_descending(BASE_BACKUP_DIR, "_BACKUP")
-
-    return
-
-
-def restore_backup(old_dir):
+def restore_archive(old_dir):
     ## Create backup directory _BEFORE_RESTORE
 
     ## copy old_dir over current
@@ -106,10 +98,8 @@ def restore_backup(old_dir):
 
 if __name__ == "__main__":
     fire.Fire({
-        "backup-current": backup_current,
-        "list-backups":   list_backups,
-        "download-new":   download_new,
-        "list-downloads": list_downloads,
-        "apply-new":      apply_new,
-        "restore-backup": restore_backup,
+        "apply-latest":    apply_latest,
+        "backup-current":  backup_current,
+        "list-archives":   list_archives,
+        "restore-archive":  restore_archive,
     })
