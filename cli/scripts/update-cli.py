528ee6131b51b735d9373484ff5fdf30a1400900
Denis Lussier <denis@lussier.io>
2024-08-10 05:17:44 -0400

    wip update-cli

--- cli/scripts/update-cli.py
+++ cli/scripts/update-cli.py
@@ -1,7 +1,9 @@
 
 #  Copyright 2022-2024 PGEDGE  All rights reserved. #
 
-import os, sys, datetime
+import os, sys, datetime, tarfile
+from urllib import request as urllib2
+
 
 import util, fire
 
@@ -19,7 +21,7 @@ def get_cli_ver(p_file):
         util.exit_message(f"Cannot locate version file: {p_file}")
 
     awk3 = "awk '{print $3}'"
-    ver_quoted = util.getoutput(f"grep 'MY_VERSION =' {p_file} | {awk3}")
+    ver_quoted = util.getoutput(f"grep 'VER =' {p_file} | {awk3}")
 
     ver = ver_quoted.replace('"', '')
 
@@ -27,16 +29,46 @@ def get_cli_ver(p_file):
 
 
 def replace_files(from_dir, to_dir):
+    util.message("DEBUG:  figure it manually first")
     return
 
 
 def download_file(p_file, p_dir):
+    file_path = f"{p_dir}/{p_file}"
+    url_path = f"{util.get_value("GLOBAL", "REPO")}/{p_file}"
+    util.message(f"Downloading {url_path} to '{file_path}'")
+
+    try:
+        fu = urllib2.urlopen(url_path)
+        local_file = open(file_path, "wb")
+        local_file.write(fu.read())
+        local_file.close()
+    except Exception as e:
+        util.exit_message(f"Unable to download \n {str(e)}")
+
     return
+
+
+def unpack_file(p_file, p_dir):
+    file_path = f"{p_dir}/{p_file}"
+    util.message(f"Unpacking {file_path} ...")
+
+    try:
+        # Use 'data' filter if available, but revert to Python 3.11 behavior ('fully_trusted')
+        #   if this feature is not available:
+        tar = tarfile.open(file_path)
+        tar.extraction_filter = getattr(tarfile, 'data_filter',
+                                       (lambda member, path: member))
+        tar.extractall()
+        tar.close()
+    except Exception as e:
+        print("ERROR: Unable to unpack \n" + str(e))
+        sys.exit(1)
 
 
 def backup_current():
     ts = get_now()
-    ver = get_cli_ver({MY_HOME}/hub/scripts/install.py)
+    ver = get_cli_ver(f"{MY_HOME}/hub/scripts/install.py")
     backup_dir = f"{BASE_BACKUP_DIR}/{ts}_{ver}_BACKUP"
     rc = os.system(f"mkdir -p {backup_dir}")
     if rc != 0:
@@ -66,19 +98,20 @@ def apply_latest():
 
     file = f"hub-{verlatest}"
     download_file(file, download_dir)
-
-    #expand_file(file, download_dir)
-
-    #replace_files(download_dir, MY_HOME)
+    unpack_file(file, download_dir)
+    replace_files(download_dir, MY_HOME)
 
     return
 
 
-def list_archives(base_dir, wildcard):
+def list_archives():
     dir_list = []
 
+    if not os.path.isdir(BASE_BACKUP_DIR):
+        os.system(f"mkdir -p {BASE_BACKUP_DIR}")
+
     for name in os.listdir(BASE_BACKUP_DIR):
-        if ["_BACKUP", "_DOWNLOAD"] in name:
+        if ("_BACKUP" in name) or ("_DOWNLOAD" in name):
             dir_list.append(name)
 
     if len(dir_list) >= 1:
