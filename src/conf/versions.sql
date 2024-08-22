f758948785c4c631896313da231c9ef7c3cacba0
Denis Lussier <denis@lussier.io>
2024-08-22 07:41:21 -0400

    pgml is for REL24_Q3

--- src/conf/versions.sql
+++ src/conf/versions.sql
@@ -74,7 +74,6 @@ INSERT INTO extensions VALUES ('snowflake', 'snowflake', 1, 'snowflake', '');
 INSERT INTO extensions VALUES ('foslots',   'foslots',   0, '',          '');
 
 INSERT INTO extensions VALUES ('vector',    'vector',       0, '',               '');
-INSERT INTO extensions VALUES ('pgml',      'pgml',         0, 'pgml',           'pgml.venv=~/venv');
 INSERT INTO extensions VALUES ('wal2json',  'wal2json',     1, 'wal2json',       '');
 INSERT INTO extensions VALUES ('timescaledb','timescaledb', 1, 'timescaledb', 'timescaledb.telemetry_level=off'); 
 INSERT INTO extensions VALUES ('permissions','pg_permissions', 0, '',               '');
@@ -312,12 +311,6 @@ INSERT INTO versions VALUES ('cron-pg16', '1.6.4-1', 'amd, arm', 1, '20230815', 
 
 INSERT INTO versions VALUES ('cron-pg15', '1.6.3-1', 'amd, arm', 0, '20230729', 'pg15', '', '');
 INSERT INTO versions VALUES ('cron-pg16', '1.6.3-1', 'amd, arm', 0, '20230729', 'pg16', '', '');
-
--- ## PGML ##############################
-INSERT INTO projects VALUES ('pgml', 'pge', 4, 0, 'hub', 1, 'https://github.com/postgresml/postgresml/tags',
-  'pgml', 1, 'pgml.png', 'GPU Powered AI Extension', 'https://github.com/postgresml/postgresml', 'postgresml');
-INSERT INTO releases VALUES ('pgml-pg16', 4, 'pgml', 'PostgresML', '', 'prod', '', 1, 'MIT', '', '');
-INSERT INTO versions VALUES ('pgml-pg16', '2.9.3-1', 'amd', 1, '20240820', 'pg16', 'UB22AMD', '');
 
 -- ## VECTOR ############################
 INSERT INTO projects VALUES ('vector', 'pge', 4, 0, 'hub', 1, 'https://github.com/pgedge/vector/tags',
