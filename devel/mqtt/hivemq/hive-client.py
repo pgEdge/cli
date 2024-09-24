1dea9361b58af8d51cb397475ba71de688df6512
Denis Lussier <pgplatform.io@lussier.io>
2024-09-24 08:54:39 -0400

    complete mqtt example with a bow

--- devel/mqtt/hivemq/hive-client.py
+++ devel/mqtt/hivemq/hive-client.py
@@ -13,29 +13,28 @@ try:
     BROKER_USER = os.getenv("BROKER_USER")
     BROKER_PASSWD = os.getenv("BROKER_PASSWD")
     TOPIC = os.getenv("TOPIC")
+    PGEDGE_HOME = os.getenv("PGEDGE_HOME")
 except Exception as e:
     print(f"ERROR loading ENV: {e}")
     sys.exit(1)
 
-# setting callbacks for different events to see if it works, print the message etc.
+def run_cli_command(cmd):
+    os.system(f"{PGEDGE_HOME}/pgedge {cmd} --json")
+
 def on_connect(client, userdata, flags, rc, properties=None):
     print("CONNACK received with code %s." % rc)
 
-# with this callback you can see if your publish was successful
 def on_publish(client, userdata, mid, properties=None):
     print("mid: " + str(mid))
 
-# print which topic was subscribed to
 def on_subscribe(client, userdata, mid, granted_qos, properties=None):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
-# print message, useful for checking if it was successful
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    payload = msg.payload
+    run_cli_command(payload.decode("utf-8"))
 
-# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
-# userdata is user defined data of any type, updated by user_data_set()
-# client_id is the given name of the client
+
 client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
 client.on_connect = on_connect
 
@@ -43,17 +42,10 @@ client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 client.username_pw_set(BROKER_USER, BROKER_PASSWD)
 client.connect(BROKER_HOST, BROKER_PORT)
 
-# setting callbacks, use separate functions like above for better visibility
 client.on_subscribe = on_subscribe
 client.on_message = on_message
 client.on_publish = on_publish
 
-# subscribe to all topics of encyclopedia by using the wildcard "#"
-client.subscribe("encyclopedia/#", qos=1)
+client.subscribe(f"{TOPIC}/#", qos=0)
 
-# a single publish, this can also be done in loops, etc.
-client.publish("encyclopedia/temperature", payload="hot", qos=1)
-
-# loop_forever for simplicity, here you need to stop the loop manually
-# you can also use loop_start and loop_stop
 client.loop_forever()
