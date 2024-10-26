6b0a6b7782ecb7ccbc64233e5391bbb95639133c
Denis Lussier <denis@lussier.io>
2024-10-26 07:28:43 -0400

    wip ai-edition

--- devel/ai-edition/ub22/install_pgedge_ai.py
+++ devel/ai-edition/ub22/install_pgedge_ai.py
@@ -1,3 +1,7 @@
 import os, sys
 
 os.system("./pgedge install pg16 --start : install pgml : install vector : install kirk")
+
+os.system("./pgedge install audit cron hintplan hypopg lolor orafce --disabled")
+
+os.system("./pgedge install partman permissions plv8 postgis setuser --disabled")
