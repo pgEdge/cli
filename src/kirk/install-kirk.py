3359ad9878e0c59a5b616946af4cbeed292f8df5
Denis Lussier <denis@lussier.io>
2024-10-29 14:22:16 -0400

    kirk looks for  ~/kirk.conf at install time

--- src/kirk/install-kirk.py
+++ src/kirk/install-kirk.py
@@ -2,6 +2,8 @@
 import os
 import util
 
+pge=f"{os.getenv('MY_HOME')}/{os.getenv('MY_CMD')}"
+
 isAutoStart = str(os.getenv("isAutoStart", "False"))
 isSTART = str(os.getenv("isSTART", "False"))
 util.message(f"install-kirk.py isAutoStart = {isAutoStart}, isSTART = {isSTART}", "debug")
@@ -12,6 +14,13 @@ util.set_column("pidfile", "kirk", pidfile)
 if isAutoStart == "True":
     util.set_column("autostart", "kirk", 1)
     isSTART = "True"
+
+conf=f"{os.getenv('HOME')}/.kirk.conf"
+util.message(f"looking for config file: {conf}")
+if os.path.isfile(conf):
+    util.message(f"running file: {conf}")
+    os.system(conf)
+
 
 if isSTART == "True":
    os.system(f"{os.getenv('MY_HOME')}/{os.getenv('MY_CMD')} start kirk")
