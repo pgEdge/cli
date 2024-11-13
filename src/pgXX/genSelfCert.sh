4dfa316c13a22043f998cfdca2be8f5cdfe3b573
Denis Lussier <denis@lussier.io>
2024-11-13 09:45:46 +0000

    explicitely set generated pg self-cert to be read-only by owner (chmod 600)

--- src/pgXX/genSelfCert.sh
+++ src/pgXX/genSelfCert.sh
@@ -9,5 +9,8 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -subj "/C=US/ST=Virginia/L=Alexandria/O=self/OU=signed/CN=localhost"\
   -keyout server.key -out server.crt > /dev/null 2>&1
 
+chmod 600 server.key
+chmod 600 server.crt
+
 cp server.crt root.crt
 
