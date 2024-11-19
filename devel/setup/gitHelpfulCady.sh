cbcae3d5a72b72e69fd61bb27267bebd65d8fbf0
cmotyka <clmotyka@gmail.com>
2024-11-19 14:59:03 +0000

    drop scratch file

--- devel/setup/gitHelpfulCady.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.name "cmotyka"
-git config --global user.email clmotyka@gmail.com
-
-git config --global push.default simple
-git config --global credential.helper store
-git config --global pull.rebase false
