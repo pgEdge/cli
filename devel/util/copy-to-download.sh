dacb9353017ba89f780affe1fa556c23b2088f60
hayee-bhatti <hayee@pgedge.com>
2025-02-07 02:31:46 +0500

    Enhanced various copy-to-bucket scripts to check for the offline bundle and if present, ensure it is copied with content-disposition headers.

--- devel/util/copy-to-download.sh
+++ devel/util/copy-to-download.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+source $PGE/env.sh
+
 export BUCKET=s3://pgedge-download
+
+# Offline bundle name
+offline_tgz_bndl="pgedge-$hubVV-$OS.tgz"
 
 if [ "$1" == "" ]; then
   echo "The outDir parameter must be specified"
@@ -22,12 +27,21 @@ ls
 sleep 2
 
 flags="--acl public-read --storage-class STANDARD --recursive"
-cmd="aws --region $REGION s3 cp . $BUCKET/REPO $flags"
-echo $cmd
-sleep 3
+BR=$BUCKET/REPO
+set -x
 
-$cmd
+aws --region $REGION s3 cp . $BR $flags
 rc=$?
-echo "rc=($rc)"
-exit $rc
+sleep 2
 
+# Additional check for offline bundle, if exists then re-upload with
+# content disposition header
+if [ $rc -eq 0 ] && [ -f "$offline_tgz_bndl" ]; then
+    echo "Uploading offline bundle with content-disposition header"
+    aws --region $REGION s3 cp "$offline_tgz_bndl" "$BUCKET/REPO/" \
+        --acl public-read \
+        --content-disposition "attachment; filename=$offline_tgz_bndl"
+    rc=$?  # Capture exit code from second upload
+fi
+
+exit $rc
