f7ac95abab41e8fadc7272f629781632de9ef8a0
hayee-bhatti <hayee@pgedge.com>
2025-06-12 17:37:08 +0500

    Enhance env.sh and make_tgz.sh to integrate with recent build_to_devel.sh changes for stable/current modes handling
    - Update env.sh to define removeComponentFromOut (that is used in make_tgz.sh)
      for stripping the Spock (unstable) component in stable mode builds
    - Enhance make_tgz.sh to support stable/current modes
    - Align with build_to_devel.sh for a consistent mode based build process

--- make_tgz.sh
+++ make_tgz.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+# Script to generate pgedge builds and the corresponding tgz bundles with mode-based customization.
+# Supports '-m MODE' switch for 'stable' (default) or 'current' modes.
+# In 'stable' mode, after generating builds, strips specified unstable components using 'removeComponentFromOut' from env.sh.
+# In 'current' mode, includes all components.
+
 TGZ_REPO="https://pgedge-download.s3.amazonaws.com/REPO"
 
 source env.sh
@@ -18,6 +23,33 @@ cmd () {
 
 }
 
+show_help() {
+  cat << EOF
+Usage: $0 [OPTIONS]
+  -m MODE         Build mode: 'stable' (default) or 'current'
+  -h              Show help
+
+Examples:
+  $0
+  $0 -m current
+EOF
+}
+
+MODE="stable"
+while getopts "m:h" opt; do
+  case $opt in
+    m) MODE="$OPTARG" ;;
+    h) show_help; exit 0 ;;
+    \?) show_help; exit 1 ;;
+  esac
+done
+# check that the mode passed is either stable or current
+if [[ "$MODE" != "stable" && "$MODE" != "current" ]]; then
+  echo "[ERROR] Invalid mode: $MODE"
+  show_help
+  exit 1
+fi
+
 ## MAINLINE ###################################
 
 vers="15 16 17"
@@ -26,6 +58,23 @@ for ver in ${vers}; do
   echo ""
   cmd "./build_all.sh $ver"
 done
+
+sleep 5
+# If mode is 'stable' and removeComponentFromOut has a value defined in env.sh,
+# strip the corresponding unstable component packages (e.g., spock50) from the build.
+# This ensures only stable components are included in the tgz bundle.
+# In 'current' mode, this step is skipped, preserving all stable and unstable components
+# Wildcards (*) in removeComponentFromOut are not supported.
+removeComponentFromOut="$(echo -n "$removeComponentFromOut" | xargs)"
+if [[ "$MODE" == "stable" && -n "$removeComponentFromOut" ]]; then
+  if [[ "$removeComponentFromOut" == *"*"* ]]; then
+    echo "[ERROR] removeComponentFromOut should not contain a wildcard (*). Skipping removal."
+  else
+    echo "[INFO] [STABLE MODE] Attempting to remove files matching: $OUT/${removeComponentFromOut}*.tgz"
+    removed_files=$(rm -vf "$OUT/${removeComponentFromOut}"*.* 2>&1 | wc -l)
+    echo "[INFO] Removed $removed_files files for $removeComponentFromOut."
+  fi
+fi
 
 ./bp.sh
 
