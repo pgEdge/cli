b5885c4b046d26f431e25bc4202a7ea8a528fbc1
asifnaeem <anaeem.it@gmail.com>
2025-06-26 01:07:39 +0500

    Support custom data directory via pg_data flag (#324)
    * CLI feature -datadir to pass absolute path for data directory to pgedge setup and cluster commands
    
    * adjustments based on review (PR#324)
    
    * more adjustments based on review (PR#324)
    
    * more adjustments based on review (PR#324)
    
    * done some cleanup (PR#324)
    
    * ensure fallback for pg_data
    
    * fix helptext in setup
    
    ---------
    
    Co-authored-by: Matthew Mols <matt@pgedge.com>

--- cli/scripts/setup.py
+++ cli/scripts/setup.py
@@ -11,16 +11,17 @@ import fire, util, db, setup_core
 CTL="./pgedge"
 
 
-def setup_pgedge(User=None, Passwd=None, dbName=None, port=None, pg_ver=None, spock_ver=None, autostart=False, interactive=False, yes=False):
+def setup_pgedge(User=None, Passwd=None, dbName=None, port=None, pg_data=None, pg_ver=None, spock_ver=None, autostart=False, interactive=False, yes=False):
     """Install a pgEdge node (including PostgreSQL, spock, and snowflake-sequences).
 
        Install a pgEdge node (including PostgreSQL, spock, and snowflake-sequences).
 
-       Example: ./pgedge setup -U user -P passwd -d test --pg_ver 16
+       Example: ./pgedge setup -U admin -P passwd -d defaultdb --pg_ver 16
        :param User: The database user that will own the db (required)
        :param Passwd: The password for the newly created db user (required)
        :param dbName: The database name (required)
        :param port: Defaults to 5432 if not specified
+       :param pg_data: The data directory to use for PostgreSQL. Must be an absolute path. Defaults to data/pgV, relative to where the CLI is installed
        :param pg_ver: Defaults to latest prod version of pg, such as 16.  May be pinned to a specific pg version such as 16.4
        :param spock_ver: Defaults to latest prod version of spock, such as 4.0.  May be pinned to a specific spock version such as 4.0.1
        :param autostart: Defaults to False
@@ -54,7 +55,7 @@ def setup_pgedge(User=None, Passwd=None, dbName=None, port=None, pg_ver=None, sp
         port = pgePort
 
     util.message(f"""
-setup.pgedge(User={User}, Passwd={Passwd}, dbName={dbName}, port={port}, pg_ver={pg_ver},
+setup.pgedge(User={User}, Passwd={Passwd}, dbName={dbName}, port={port}, pg_data={pg_data}, pg_ver={pg_ver},
   spock_ver={spock_ver}, autostart={autostart}, interactive={interactive}, yes={yes})
 """, "debug")
 
@@ -95,8 +96,17 @@ setup.pgedge(User={User}, Passwd={Passwd}, dbName={dbName}, port={port}, pg_ver=
 
     pg_major, pg_minor = setup_core.parse_pg(pg_ver)
 
+    pg_init_options = ""
+    if pg_data is not None:
+        pg_data = pg_data.rstrip("/")
+        if not os.path.isabs(pg_data):
+            util.exit_message(
+                "pg_data cannot be set as relative path. Please specify absolute path instead"
+            )
+        pg_init_options = f"--datadir={pg_data}"
+
     setup_core.check_pre_reqs(
-        User, Passwd, dbName, port, pg_major, pg_minor, spock_ver, autostart)
+        User, Passwd, dbName, port, pg_data, pg_major, pg_minor, spock_ver, autostart)
 
     if interactive and yes is False:
         y_or_n = input("Do you want to continue? [Y/n] ")
@@ -119,7 +129,7 @@ setup.pgedge(User={User}, Passwd={Passwd}, dbName={dbName}, port={port}, pg_ver=
         if autostart is True:
             util.autostart_config(pg_maj)
         else:
-            setup_core.osSys(f"{CTL} init {pg_maj}")
+            setup_core.osSys(f"{CTL} init {pg_maj} {pg_init_options}")
 
         setup_core.osSys(f"{CTL} config {pg_maj} --port={port}")
 
