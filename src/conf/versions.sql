1da99a248f49b41b317a240b2f101a7b81dafba9
Matthew Mols <matt@pgedge.com>
2025-08-19 10:29:06 -0500

    remove 4.0.10-2 from versions.sql

--- src/conf/versions.sql
+++ src/conf/versions.sql
@@ -328,10 +328,6 @@ INSERT INTO releases VALUES ('spock40-pg15', 4, 'spock', 'Spock', '', 'prod', ''
 INSERT INTO releases VALUES ('spock40-pg16', 4, 'spock', 'Spock', '', 'prod', '', 1, 'pgEdge Community', '', '');
 INSERT INTO releases VALUES ('spock40-pg17', 4, 'spock', 'Spock', '', 'prod', '', 1, 'pgEdge Community', '', '');
 
-INSERT INTO versions VALUES ('spock40-pg15', '4.0.10-2', 'amd, arm', 1, '20250815', 'pg15', '', '');
-INSERT INTO versions VALUES ('spock40-pg16', '4.0.10-2', 'amd, arm', 1, '20250815', 'pg16', '', '');
-INSERT INTO versions VALUES ('spock40-pg17', '4.0.10-2', 'amd, arm', 1, '20250815', 'pg17', '', '');
-
 INSERT INTO versions VALUES ('spock40-pg15', '4.0.10-1', 'amd, arm', 0, '20250224', 'pg15', '', '');
 INSERT INTO versions VALUES ('spock40-pg16', '4.0.10-1', 'amd, arm', 0, '20250224', 'pg16', '', '');
 INSERT INTO versions VALUES ('spock40-pg17', '4.0.10-1', 'amd, arm', 0, '20250224', 'pg17', '', '');
