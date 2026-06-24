fb44ba3e308135680b4410ed29548f22ca2de391
hayee-bhatti <hayee@pgedge.com>
2026-06-24 13:48:39 +0000

    Updating spock version to 5.0.10

--- src/conf/versions.sql
+++ src/conf/versions.sql
@@ -349,9 +349,9 @@ INSERT INTO releases VALUES ('spock50-pg15', 4, 'spock', 'Spock', '', 'prod', ''
 INSERT INTO releases VALUES ('spock50-pg16', 4, 'spock', 'Spock', '', 'prod', '', 1, 'POSTGRES', '', '');
 INSERT INTO releases VALUES ('spock50-pg17', 4, 'spock', 'Spock', '', 'prod', '', 1, 'POSTGRES', '', '');
 
-INSERT INTO versions VALUES ('spock50-pg15', '5.0.10-rc.1-1',   'amd', 1, '20260624', 'pg15', '', '');
-INSERT INTO versions VALUES ('spock50-pg16', '5.0.10-rc.1-1',   'amd', 1, '20260624', 'pg16', '', '');
-INSERT INTO versions VALUES ('spock50-pg17', '5.0.10-rc.1-1',   'amd', 1, '20260624', 'pg17', '', '');
+INSERT INTO versions VALUES ('spock50-pg15', '5.0.10-1',   'amd', 1, '20260624', 'pg15', '', '');
+INSERT INTO versions VALUES ('spock50-pg16', '5.0.10-1',   'amd', 1, '20260624', 'pg16', '', '');
+INSERT INTO versions VALUES ('spock50-pg17', '5.0.10-1',   'amd', 1, '20260624', 'pg17', '', '');
 
 INSERT INTO versions VALUES ('spock50-pg15', '5.0.9-1',   'amd', 0, '20260609', 'pg15', '', '');
 INSERT INTO versions VALUES ('spock50-pg16', '5.0.9-1',   'amd', 0, '20260609', 'pg16', '', '');
